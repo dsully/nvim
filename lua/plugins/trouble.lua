c7ea5052a9aafe1f6e7ff4b0aa8baad24ce2d33a
Dan Sully <dan+github@sully.org>
2023-06-30 16:27:11 -0700

    Add Trouble cmd loaders.

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,5 +1,11 @@
 return {
     "folke/trouble.nvim",
+    cmd = {
+        "Trouble",
+        "TroubleClose",
+        "TroubleRefresh",
+        "TroubleToggle",
+    },
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
         {
