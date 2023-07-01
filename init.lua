401b0853eee321cb525cfdcde4bf33a232ed711a
Dan Sully <dan+github@sully.org>
2023-07-01 12:24:48 -0700

    Tweaks to large file loading.
    Disable smart case & smart indent.

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ if vim.loader then
     vim.loader.enable()
 end
 
-require("config.autocommands")
-require("config.keymaps")
 require("config.options")
+require("config.keymaps")
+require("config.autocommands")
 require("config.lazy")
