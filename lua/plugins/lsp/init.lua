a6af33e67e556e1a06245f1c877b5fd6ac10ed86
Dan Sully <dsully@users.noreply.github.com>
2023-08-08 10:28:11 -0700

    LSP: Set tsserver_path for typescript-tools.

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -388,6 +388,9 @@ local servers = {
     },
 
     tsserver = function()
+        local mason_registry = require("mason-registry")
+        local tsserver_path = mason_registry.get_package("typescript-language-server"):get_install_path()
+
         require("typescript-tools").setup({
             capabilities = common.capabilities(),
             on_attach = common.on_attach,
@@ -408,6 +411,7 @@ local servers = {
                     trimTrailingWhitespace = false,
                     semicolons = "insert",
                 },
+                tsserver_path = tsserver_path .. "/node_modules/typescript/lib/tsserver.js",
             },
         })
     end,
