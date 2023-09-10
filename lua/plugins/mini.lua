78d6d8a75c0236c4c4ebf4ca393a0d2692cbffd3
Dan Sully <dan+github@sully.org>
2023-09-09 21:16:19 -0700

    mini.pairs: Enable.

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -22,12 +22,13 @@ return {
     {
         "echasnovski/mini.comment",
         keys = {
-            { "gc", mode = { "n", "x" } },
-            { "gcc", mode = { "n", "x" } },
+            { "gc", mode = { "n", "x" }, desc = "Comment Line(s)" },
+            { "gcc", mode = { "n", "x" }, desc = "Uncomment Line(s)" },
         },
         opts = true,
     },
     {
+        -- Show hex colors as colors.
         "echasnovski/mini.hipatterns",
         config = function()
             require("mini.hipatterns").setup({
@@ -37,6 +38,26 @@ return {
             })
         end,
         event = "BufReadPre",
+    },
+    {
+        "echasnovski/mini.pairs",
+        event = "InsertEnter",
+        opts = {
+            mappings = {
+                -- source: https://old.reddit.com/r/neovim/comments/163rzex/how_to_avoid_autocompleting_right_parentheses/jy4zwp8/
+                -- disable if a matching character is in an adjacent position (eg. fixes
+                -- markdown triple ticks) neigh_pattern: a pattern for *two* neighboring
+                -- characters (before and after). Use dot `.` to allow any character.
+                -- Here, we disable the functionality instead of inserting a matching quote
+                -- if there is an adjacent non-space character
+                ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^%S][^%S]", register = { cr = false } },
+                ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^%S][^%S]", register = { cr = false } },
+                ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%S][^%S]", register = { cr = false } },
+                ["["] = { action = "closeopen", pair = "[]", neigh_pattern = "[^%S][^%S]", register = { cr = false } },
+                ["{"] = { action = "closeopen", pair = "{}", neigh_pattern = "[^%S][^%S]", register = { cr = false } },
+                ["("] = { action = "closeopen", pair = "()", neigh_pattern = "[^%S][^%S]", register = { cr = false } },
+            },
+        },
     },
     {
         "echasnovski/mini.surround",
@@ -50,6 +71,6 @@ return {
                 },
             })
         end,
-        keys = { "gz" },
+        keys = { { "gz", desc = "+ Surround" } },
     },
 }
