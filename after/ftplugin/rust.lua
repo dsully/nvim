82c925b8319040246cc7d3881b82975d0f5c1381
Dan Sully <dsully@users.noreply.github.com>
2023-09-09 21:16:19 -0700

    mini.pairs: Enable.

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -15,3 +15,14 @@ vim.cmd.compiler("cargo")
 vim.keymap.set("n", "<localleader>t", "<cmd>make test -q<cr>", { desc = "Cargo test" })
 vim.keymap.set("n", "<localleader>b", "<cmd>make build<cr>", { desc = "Cargo build" })
 vim.keymap.set("n", "<localleader>c", "<cmd>make clippy -q<cr>", { desc = "Cargo clippy" })
+
+--
+if package.loaded["mini.pairs"] then
+    local mp = require("mini.pairs")
+
+    mp.map_buf(0, "i", "<", { action = "open", pair = "<>", neigh_pattern = "[%a:].", register = { cr = false } })
+    mp.map_buf(0, "i", ">", { action = "close", pair = "<>", register = { cr = false } })
+
+    -- Don't close single quote if we're in a lifetime position.
+    mp.map_buf(0, "i", "'", { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\<&].", register = { cr = false } })
+end
