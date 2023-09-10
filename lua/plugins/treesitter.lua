0a4c52149d859c924b1aef27bb07f5a53a73bb33
Dan Sully <dsully@users.noreply.github.com>
2023-09-09 21:48:54 -0700

    Move mini.ai and update it's config.
    Remove textobjects & textsubjets.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -146,39 +146,6 @@ return {
                     lint_events = { "BufWrite", "CursorHold" },
                 },
 
-                textobjects = {
-                    lsp_interop = {
-                        enable = true,
-                        border = vim.g.border,
-                        peek_definition_code = {
-                            ["<leader>cf"] = { query = "@function.outer", desc = "Peek Function Outer" },
-                            ["<leader>cF"] = { query = "@class.outer", desc = "Peek Class Outer " },
-                        },
-                    },
-                    move = {
-                        enable = true,
-                        goto_next_start = {
-                            ["]f"] = { query = "@function.outer", desc = "Next function start." },
-                        },
-                        goto_previous_start = {
-                            ["[f"] = { query = "@function.outer", desc = "Previous function start." },
-                        },
-                    },
-                },
-
-                -- https://github.com/RRethy/nvim-treesitter-textsubjects
-                textsubjects = {
-                    enable = true,
-                    -- keymap to select the previous selection
-                    prev_selection = ",",
-                    -- These are all visual mode.
-                    keymaps = {
-                        ["."] = "textsubjects-smart",
-                        ["[["] = "textsubjects-container-outer",
-                        ["]]"] = "textsubjects-container-inner",
-                    },
-                },
-
                 -- https://github.com/yioneko/nvim-yati
                 yati = {
                     enable = true,
@@ -193,38 +160,6 @@ return {
 
     { "RRethy/nvim-treesitter-endwise", event = "InsertEnter" },
     { "yioneko/nvim-yati", event = "InsertEnter" },
-
-    {
-        "echasnovski/mini.ai",
-        config = function()
-            local spec_treesitter = require("mini.ai").gen_spec.treesitter
-
-            require("mini.ai").setup({
-                custom_textobjects = {
-                    o = spec_treesitter({
-                        a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-                        i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-                    }, {}),
-                    f = spec_treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-                    c = spec_treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
-                },
-                n_lines = 500,
-            })
-        end,
-        dependencies = {
-            {
-                -- no need to load the plugin, since we only need its queries
-                "nvim-treesitter/nvim-treesitter-textobjects",
-                init = function()
-                    require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-                end,
-            },
-        },
-        keys = {
-            { "a", mode = { "x", "o" } },
-            { "i", mode = { "x", "o" } },
-        },
-    },
 
     -- Use treesitter to auto-close and auto-rename HTML tags.
     { "windwp/nvim-ts-autotag", ft = { "html", "javascript", "markdown", "xml" }, opts = {} },
