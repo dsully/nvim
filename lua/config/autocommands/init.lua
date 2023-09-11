2bde8a5c50c63ce70e87ae681da96ccf5c921bf7
Dan Sully <dan+github@sully.org>
2023-09-10 20:47:51 -0700

    autocommands: Delete hidden no-name buffers.

--- lua/config/autocommands/init.lua
+++ lua/config/autocommands/init.lua
@@ -37,6 +37,17 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     end,
 })
 
+vim.api.nvim_create_autocmd({ "BufHidden" }, {
+    desc = "Delete [No Name] buffers",
+    callback = function(event)
+        if event.file == "" and vim.bo[event.buf].buftype == "" and not vim.bo[event.buf].modified then
+            vim.schedule(function()
+                pcall(vim.api.nvim_buf_delete, event.buf, {})
+            end)
+        end
+    end,
+})
+
 vim.api.nvim_create_autocmd({ "BufReadCmd" }, {
     callback = function(args)
         vim.cmd.bdelete({ args.buf, bang = true })
