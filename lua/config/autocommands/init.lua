9b10079bae200dfe8bd7a20a189daa585055d51f
Dan Sully <dan+github@sully.org>
2023-09-10 21:24:22 -0700

    autocommands: Comment out buffer deletion.
    Might be causing slowdowns.

--- lua/config/autocommands/init.lua
+++ lua/config/autocommands/init.lua
@@ -37,16 +37,16 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     end,
 })
 
-vim.api.nvim_create_autocmd({ "BufHidden" }, {
-    desc = "Delete [No Name] buffers",
-    callback = function(event)
-        if event.file == "" and vim.bo[event.buf].buftype == "" and not vim.bo[event.buf].modified then
-            vim.schedule(function()
-                pcall(vim.api.nvim_buf_delete, event.buf, {})
-            end)
-        end
-    end,
-})
+-- vim.api.nvim_create_autocmd({ "BufHidden" }, {
+--     desc = "Delete [No Name] buffers",
+--     callback = function(event)
+--         if event.file == "" and vim.bo[event.buf].buftype == "" and not vim.bo[event.buf].modified then
+--             vim.schedule(function()
+--                 pcall(vim.api.nvim_buf_delete, event.buf, {})
+--             end)
+--         end
+--     end,
+-- })
 
 vim.api.nvim_create_autocmd({ "BufReadCmd" }, {
     callback = function(args)
