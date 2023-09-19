845fd16cd57428459f690b8e8a3cbdd6104a9813
Dan Sully <dsully@users.noreply.github.com>
2023-09-19 14:49:35 -0700

    dmypyls: Use --chdir argument for proper type checking.

--- lua/lspconfig/server_configurations/dmypyls.lua
+++ lua/lspconfig/server_configurations/dmypyls.lua
@@ -1,19 +1,37 @@
+local root_dir = function(fname)
+    local lsputil = require("lspconfig.util")
+
+    local markers = {
+        "Pipfile",
+        "pyproject.toml",
+        "pyrightconfig.json",
+        "setup.py",
+        "setup.cfg",
+        "requirements.txt",
+    }
+
+    return lsputil.root_pattern(unpack(markers))(fname) or lsputil.find_git_ancestor(fname) or lsputil.path.dirname(fname)
+end
+
+local find_command = function()
+    local gcommand = "dmypy-ls"
+
+    if vim.env.VIRTUAL_ENV then
+        local vcommand = vim.env.VIRTUAL_ENV .. "/bin/" .. gcommand
+
+        if vim.uv.fs_stat(vcommand) then
+            gcommand = vcommand
+        end
+    end
+
+    return { gcommand, "--chdir", root_dir(vim.uv.cwd()) }
+end
+
 return {
     default_config = {
-        cmd = { "dmypy-ls" },
+        cmd = find_command(),
         filetypes = { "python" },
-        root_dir = function(fname)
-            local lsputil = require("lspconfig.util")
-            local markers = {
-                "Pipfile",
-                "pyproject.toml",
-                "pyrightconfig.json",
-                "setup.py",
-                "setup.cfg",
-                "requirements.txt",
-            }
-            return lsputil.root_pattern(unpack(markers))(fname) or lsputil.find_git_ancestor(fname) or lsputil.path.dirname(fname)
-        end,
+        root_dir = root_dir,
         single_file_support = true,
     },
 }
