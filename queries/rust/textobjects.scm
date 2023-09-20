4b9dba427e55fe18599d4c2f464d854bb661b996
Dan Sully <dan+github@sully.org>
2023-09-20 06:39:35 -0700

    Rust: Add textobjects query.

---
+++ queries/rust/textobjects.scm
@@ -0,0 +1,8 @@
+;extends
+
+((call_expression
+  function: (field_expression
+    value: (_) "." @_start
+    field: (field_identifier) @method.inner)
+  arguments: (arguments) @_end)
+  (#make-range! "method.outer" @_start @_end))
