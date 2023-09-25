44607508a80a2dd8e6eabdf9bb62c878fbf07afd
Dan Sully <dsully@users.noreply.github.com>
2023-09-25 06:59:01 -0700

    mini.pairs: Update config. Add Python override.

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -36,3 +36,9 @@ vim.api.nvim_create_autocmd("InsertCharPre", {
         vim.api.nvim_input("<Esc>m'" .. row + 1 .. "gg" .. col + 1 .. "|if<Esc>`'la")
     end,
 })
+
+if package.loaded["mini.pairs"] then
+    --
+    -- Don't match on the 3rd quote for docstrings.
+    require("mini.pairs").map_buf(0, "i", '"', { action = "closeopen", pair = '""', neigh_pattern = '[^\\"][]%s)}\'"]', register = { cr = false } })
+end
