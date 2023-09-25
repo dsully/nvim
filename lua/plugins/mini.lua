fa686aa5406d823581d02bd6052d473054f3fae8
Dan Sully <dan+github@sully.org>
2023-09-25 06:59:01 -0700

    mini.pairs: Update config. Add Python override.

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -182,18 +182,16 @@ return {
         event = "InsertEnter",
         opts = {
             mappings = {
-                -- source: https://old.reddit.com/r/neovim/comments/163rzex/how_to_avoid_autocompleting_right_parentheses/jy4zwp8/
-                -- disable if a matching character is in an adjacent position (eg. fixes
-                -- markdown triple ticks) neigh_pattern: a pattern for *two* neighboring
-                -- characters (before and after). Use dot `.` to allow any character.
-                -- Here, we disable the functionality instead of inserting a matching quote
-                -- if there is an adjacent non-space character
-                ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^%S][^%S]", register = { cr = false } },
-                ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^%S][^%S]", register = { cr = false } },
-                ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%S][^%S]", register = { cr = false } },
-                ["["] = { action = "closeopen", pair = "[]", neigh_pattern = "[^%S][^%S]", register = { cr = false } },
-                ["{"] = { action = "closeopen", pair = "{}", neigh_pattern = "[^%S][^%S]", register = { cr = false } },
-                ["("] = { action = "closeopen", pair = "()", neigh_pattern = "[^%S][^%S]", register = { cr = false } },
+                ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\][]%s)}'\"]" },
+                ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\][]%s)}'\"]" },
+                ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\][]%s)}'\"]" },
+
+                ["<"] = { action = "open", pair = "<>", neigh_pattern = "[^\\][%s]" },
+                [">"] = { action = "close", pair = "<>", neigh_pattern = "[^\\]." },
+
+                ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\][]%s)}'\"]", register = { cr = false } },
+                ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\][]%s)}'\"]", register = { cr = false } },
+                ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\][]%s)}'\"]", register = { cr = false } },
             },
         },
     },
