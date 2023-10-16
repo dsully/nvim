ede291e4ea6869c730c10f2053bc1d5de47beaea
Dan Sully <dan+github@sully.org>
2023-10-16 09:33:17 -0700

    coding: Update muren config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -27,5 +27,15 @@ return {
     { "hjdivad/vim-pdl", ft = "pdl" },
 
     -- Pattern replacement UI.
-    { "AckslD/muren.nvim", event = "VeryLazy", opts = {} },
+    {
+        "AckslD/muren.nvim",
+        event = { { "BufNewFile", "BufAdd" } },
+        opts = {
+            patterns_width = 60,
+            patterns_height = 20,
+            options_width = 40,
+            preview_height = 24,
+        },
+        cmd = "MurenToggle",
+    },
 }
