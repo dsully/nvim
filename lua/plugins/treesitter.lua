fc0cc15ca59b475af8c4a49dbe75c159e64f8188
Dan Sully <dan+github@sully.org>
2023-10-26 08:59:07 -0700

    treesitter: Performance improvement from lazyvim.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -117,6 +117,14 @@ return {
             vim.keymap.set("n", "<leader>i", vim.show_pos, { desc = "Inspect Position" })
         end,
         event = vim.g.defaults.lazyfile,
+        init = function(plugin)
+            -- From Lazyvim: Add nvim-treesitter queries to the rtp and it's custom query predicates early
+            -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+            -- no longer trigger the **nvim-treeitter** module to be loaded in time.
+            -- Luckily, the only thins that those plugins need are the custom queries, which we make available during startup.
+            require("lazy.core.loader").add_to_rtp(plugin)
+            require("nvim-treesitter.query_predicates")
+        end,
     },
 
     -- Better % matching.
