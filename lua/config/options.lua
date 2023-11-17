781a0301ec0c89149d98567f536118aebf71e0e5
Dan Sully <dan+github@sully.org>
2023-11-16 20:40:28 -0800

    Update OSC52 config.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -96,12 +96,12 @@ if vim.env.SSH_CONNECTION then
     vim.g.clipboard = {
         name = "OSC 52",
         copy = {
-            ["+"] = require("vim.ui.clipboard.osc52").copy,
-            ["*"] = require("vim.ui.clipboard.osc52").copy,
+            ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+            ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
         },
         paste = {
-            ["+"] = require("vim.ui.clipboard.osc52").paste,
-            ["*"] = require("vim.ui.clipboard.osc52").paste,
+            ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+            ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
         },
     }
 elseif vim.g.os == "Darwin" then
