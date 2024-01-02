c169fbb12f1c3750208b71a167c1fc11a2760046
Dan Sully <dsully@users.noreply.github.com>
2024-01-01 20:14:57 -0800

    snippets: Remove unneeded.

--- lua/snippets/all.lua
+++ lua/snippets/all.lua
@@ -2,36 +2,13 @@ local luasnip = require("luasnip")
 
 local c = luasnip.choice_node
 local d = luasnip.dynamic_node
-local f = luasnip.function_node
-local i = luasnip.insert_node
 local s = luasnip.snippet
 local t = luasnip.text_node
 local sn = luasnip.snippet_node
 
-local date = function()
-    return {
-        os.date("%Y-%m-%d"),
-    }
-end
-
 -- documentation for snippet format inside examples:
 -- https://github.com/L3MON4D3/LuaSnip/blob/master/Examples/snippets.lua
 return {
-    s(
-        "curtime",
-        f(function()
-            return os.date("%D - %H:%M")
-        end)
-    ),
-
-    s({
-        trig = "today",
-        name = "Today",
-        dscr = "Today's date in YYYY-MM-DD",
-    }, {
-        t(date()),
-    }),
-
     s("todo", {
         c(1, {
             t("TODO(dsully): "),
