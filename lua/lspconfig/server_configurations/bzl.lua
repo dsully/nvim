bc0c7cbae3b6999dfdd184362727d1bcbf8febe7
Dan Sully <dan+github@sully.org>
2024-01-19 07:05:16 -0800

    bzl is now in lspconfig

--- lua/lspconfig/server_configurations/bzl.lua
+++
@@ -1,8 +0,0 @@
-return {
-    default_config = {
-        cmd = { "bzl", "lsp", "serve" },
-        filetypes = { "bazel" },
-        root_dir = require("lspconfig.util").root_pattern("WORKSPACE", "WORKSPACE.bazel", "MODULE.bazel"),
-        single_file_support = true,
-    },
-}
