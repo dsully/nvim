a508466102fd67b3ac5fe02602660b3d1b636da8
Dan Sully <dan+github@sully.org>
2024-02-04 06:13:08 -0800

    treesitter: Update config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,3 @@
--- Treesitter and extensions.
 return {
     {
         "nvim-treesitter/nvim-treesitter",
@@ -10,11 +9,15 @@ return {
             "TSEnable",
             "TSInstall",
             "TSModuleInfo",
+            "TSUpdate",
             "TSUpdateSync",
         },
-        config = function()
+        ---@param opts TSConfig
+        config = function(_, opts)
             local parser = require("nvim-treesitter.parsers").get_parser_configs()
 
+            ---@type ParserInfo
+            ---@diagnostic disable-next-line: inject-field
             parser.caddy = {
                 install_info = {
                     url = "https://github.com/Samonitari/tree-sitter-caddy",
@@ -23,8 +26,11 @@ return {
                     revision = "65b60437983933d00809c8927e7d8a29ca26dfa3",
                 },
                 filetype = "caddyfile",
+                maintainers = {},
             }
 
+            ---@type ParserInfo
+            ---@diagnostic disable-next-line: inject-field
             parser.gotmpl = {
                 install_info = {
                     url = "https://github.com/ngalaiko/tree-sitter-go-template",
@@ -32,6 +38,7 @@ return {
                     revision = "45acf03891557b80a45ac1897e2cca2e8b9cf0ff",
                 },
                 filetype = "gotmpl",
+                maintainers = {},
                 used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl" },
             }
 
@@ -39,84 +46,103 @@ return {
             vim.treesitter.language.register("bash", "sh")
             vim.treesitter.language.register("ruby", "Brewfile")
 
-            require("nvim-treesitter.configs").setup({
+            if type(opts.ensure_installed) == "table" then
+                ---@type table<string, boolean>
+                local added = {}
 
-                ensure_installed = {
-                    "bash",
-                    "c",
-                    "caddy",
-                    "cmake",
-                    "comment",
-                    "cpp",
-                    "css",
-                    "diff",
-                    "dockerfile",
-                    "fish",
-                    "git_config",
-                    "git_rebase",
-                    "gitignore",
-                    "go",
-                    "gomod",
-                    "gotmpl",
-                    "graphql",
-                    "groovy",
-                    "hcl",
-                    "html",
-                    "htmldjango",
-                    "http",
-                    "ini",
-                    "java",
-                    "javascript",
-                    "jsdoc",
-                    "json",
-                    "jsonc",
-                    "kdl",
-                    "kotlin",
-                    "lua",
-                    "luadoc",
-                    "luap",
-                    "make",
-                    "markdown",
-                    "markdown_inline",
-                    "mermaid",
-                    "passwd",
-                    "proto",
-                    "python",
-                    "query",
-                    "regex",
-                    "requirements",
-                    "ron",
-                    "rst",
-                    "ruby",
-                    "rust",
-                    "ssh_config",
-                    "strace",
-                    "swift",
-                    "teal",
-                    "textproto",
-                    "toml",
-                    "tsx",
-                    "typescript",
-                    "vim",
-                    "vimdoc",
-                    "yaml",
-                    "xml",
-                },
+                opts.ensure_installed = vim.tbl_filter(function(lang)
+                    if added[lang] then
+                        return false
+                    end
+                    added[lang] = true
+                    return true
+                end, opts.ensure_installed) ---@diagnostic disable-line: param-type-mismatch
+            end
 
-                highlight = { enable = true },
-
-                indent = { enable = true },
-
-                query_linter = {
-                    enable = true,
-                    use_virtual_text = true,
-                    lint_events = { "BufWrite", "CursorHold" },
-                },
-            })
-
-            vim.keymap.set("n", "<leader>i", vim.show_pos, { desc = "Inspect Position" })
+            require("nvim-treesitter.configs").setup(opts)
         end,
-        event = vim.g.defaults.lazyfile,
+        event = { "LazyFile", "VeryLazy" },
+        init = function(plugin)
+            require("lazy.core.loader").add_to_rtp(plugin)
+            require("nvim-treesitter.query_predicates")
+        end,
+        keys = {
+            { "<leader>i", vim.show_pos, desc = "Inspect Position" },
+        },
+        ---@type TSConfig
+        ---@diagnostic disable-next-line: missing-fields
+        opts = {
+            ensure_installed = {
+                "bash",
+                "c",
+                "caddy",
+                "cmake",
+                "comment",
+                "cpp",
+                "css",
+                "diff",
+                "dockerfile",
+                "fish",
+                "git_config",
+                "git_rebase",
+                "gitignore",
+                "go",
+                "gomod",
+                "gotmpl",
+                "graphql",
+                "groovy",
+                "hcl",
+                "html",
+                "htmldjango",
+                "http",
+                "ini",
+                "java",
+                "javascript",
+                "jsdoc",
+                "json",
+                "jsonc",
+                "kdl",
+                "kotlin",
+                "lua",
+                "luadoc",
+                "luap",
+                "make",
+                "markdown",
+                "markdown_inline",
+                "mermaid",
+                "passwd",
+                "proto",
+                "python",
+                "query",
+                "regex",
+                "requirements",
+                "rst",
+                "ruby",
+                "ssh_config",
+                "strace",
+                "swift",
+                "teal",
+                "textproto",
+                "ron",
+                "rust",
+                "toml",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "yaml",
+                "xml",
+            },
+            autotag = { enable = true },
+            highlight = { enable = true },
+            indent = { enable = true },
+            matchup = { enable = true },
+            query_linter = {
+                enable = true,
+                use_virtual_text = true,
+                lint_events = { "BufWrite", "CursorHold" },
+            },
+        },
     },
     {
         -- Better % matching.
@@ -133,34 +159,13 @@ return {
             -- Wrong matching for HTML: https://github.com/andymass/vim-matchup/issues/19
             vim.g.matchup_matchpref = { html = { nolists = 1 } }
         end,
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            opts = function(_, opts)
-                opts.matchup = { enable = true }
-            end,
-        },
-        event = vim.g.defaults.lazyfile,
-    },
-    {
-        "RRethy/nvim-treesitter-endwise",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            opts = function(_, opts)
-                opts.endwise = { enable = true }
-            end,
-        },
-        event = vim.g.defaults.lazyfile,
+        event = "LazyFile",
     },
     {
         -- Use treesitter to auto-close and auto-rename HTML tags.
         "windwp/nvim-ts-autotag",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            opts = function(_, opts)
-                opts.autotag = { enable = true }
-            end,
-        },
         ft = { "html", "javascript", "markdown", "xml" },
+        opts = {},
     },
 
     -- f-string manipulation.
