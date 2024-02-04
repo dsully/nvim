baecb5de4c01ea73ab6878eb778909a6abe474b4
Dan Sully <dan+github@sully.org>
2024-02-04 06:29:56 -0800

    refactor: Remove backseat & nvim-fundo

--- lua/plugins/backseat.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "james1236/backseat.nvim",
-    event = "VeryLazy",
-    opts = {
-        openai_model_id = "gpt-4", -- gpt-4 when API opens up.
-    },
-}
--- lua/plugins/undo.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "kevinhwang91/nvim-fundo",
-    dependencies = { "kevinhwang91/promise-async" },
-    event = "BufRead",
-    opts = true,
-}
