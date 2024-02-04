bcc621a9862005ab3d079ea57190dce5a1316cb2
Dan Sully <dsully@users.noreply.github.com>
2024-02-04 12:17:40 -0800

    rust: Clean up after/ft a bit.

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,21 +1,3 @@
-local cmd = require("config.defaults").cmd
-vim.keymap.set("n", "<leader>ce", function()
-    ---
-    vim.lsp.buf_request(0, "experimental/openCargoToml", {
-        textDocument = vim.lsp.util.make_text_document_params(0),
-    }, function(_, result, ctx)
-        --
-        if result ~= nil then
-            vim.lsp.util.jump_to_location(result, vim.lsp.get_client_by_id(ctx.client_id).offset_encoding)
-        end
-    end)
-end, { desc = "Open Cargo.toml" })
-
-vim.cmd.compiler("cargo")
-
-vim.keymap.set("n", "<localleader>t", cmd("make test -q"), { desc = "Cargo test" })
-vim.keymap.set("n", "<localleader>b", cmd("make build"), { desc = "Cargo build" })
-vim.keymap.set("n", "<localleader>c", cmd("make clippy -q"), { desc = "Cargo clippy" })
 
 --
 if package.loaded["mini.pairs"] then
