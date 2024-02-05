22af714de88e685e8dcda156165442cb4bfda17e
Dan Sully <dsully@users.noreply.github.com>
2024-02-04 19:38:18 -0800

    nvim-notify: Update config.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -19,10 +19,9 @@ return {
             max_height = function()
                 return math.floor(vim.o.lines * 0.75)
             end,
-            max_width = function()
-                return math.floor(vim.o.columns * 0.75)
-            end,
-            minimum_width = 50,
+            max_width = 65,
+            minimum_width = 65,
+            render = "compact",
             timeout = 3000,
         },
     },
