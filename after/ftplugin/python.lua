20ee1fc055dab2dd40db8d1bae5f7c7917080a56
Dan Sully <dsully@users.noreply.github.com>
2024-02-13 04:42:16 -0800

    python: Remove mini.pairs

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,9 +1,3 @@
-if package.loaded["mini.pairs"] then
-    --
-    -- Don't match on the 3rd quote for docstrings.
-    require("mini.pairs").map_buf(0, "i", '"', { action = "closeopen", pair = '""', neigh_pattern = '[^\\"][]%s)}\'"]', register = { cr = false } })
-end
-
 -- Install dmypy-ls in virtual env if it doesn't exist.
 -- This is needed so types can be resolved correctly.
 --
