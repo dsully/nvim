fbf984eb518d12122acb1565309b36ef493dc1c2
Dan Sully <dsully@users.noreply.github.com>
2024-02-25 16:34:55 -0800

    Formatting: Format on Save

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -11,6 +11,26 @@ return {
         },
     },
     opts = {
+        format_on_save = function(bufnr)
+            -- if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+            --     return
+            -- end
+
+            -- Disable with a global or buffer-local variable
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                return
+            end
+
+            -- Disable autoformat for files in a certain path
+            local bufname = vim.api.nvim_buf_get_name(bufnr)
+
+            if bufname:match("/node_modules/") or bufname:match("/cargo/registry/") or bufname:match("site-packages") then
+                return
+            end
+
+            return { timeout_ms = 500, lsp_fallback = true }
+        end,
+
         ---@type table<string, conform.FormatterUnit[]>
         formatters_by_ft = require("config.defaults").formatters,
 
