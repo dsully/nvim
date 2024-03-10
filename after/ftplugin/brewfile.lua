ca2c0f382cc9616a53172851260bf8879bd81336
Dan Sully <dsully@users.noreply.github.com>
2024-03-10 09:23:17 -0700

    homebrew: Fix sorting & de-duplication.

--- after/ftplugin/brewfile.lua
+++ after/ftplugin/brewfile.lua
@@ -7,37 +7,36 @@ vim.api.nvim_set_hl(0, "@symbol.ruby", { link = "Special" })
 e.on(e.BufWritePre, function()
     local entries = {}
     local lines = {}
+    local categories = { "tap", "brew", "cask", "mas", "vscode" }
+
+    for _, category in ipairs(categories) do
+        entries[category] = {}
+    end
 
     -- Collect existing entries from the buffer.
     for _, line in ipairs(vim.api.nvim_buf_get_lines(0, 0, -1, true)) do
-        for category, entry in line:gmatch("(%l+) (.+)") do
-            if not entries[category] then
-                entries[category] = { entry }
-            else
-                table.insert(entries[category], entry)
+        for category, entry in line:gmatch("(%w+) (.+)") do
+            table.insert(entries[category], entry)
+        end
+    end
+
+    for _, category in ipairs(categories) do
+        local hash = {}
+
+        -- The actual sort.
+        table.sort(entries[category])
+
+        for _, item in ipairs(entries[category]) do
+            -- Ensure we don't have duplicates.
+            if not hash[item] then
+                hash[item] = true
+
+                table.insert(lines, category .. " " .. item)
             end
         end
     end
 
-    for _, category in ipairs({ "tap", "brew", "cask", "mas", "vscode" }) do
-        if entries[category] then
-            local hash = {}
-
-            -- The actual sort.
-            table.sort(entries[category])
-
-            for _, item in ipairs(entries[category]) do
-                -- Ensure we don't have duplicates.
-                if not hash[item] then
-                    hash[item] = true
-
-                    table.insert(lines, category .. " " .. item)
-                end
-            end
-        end
-    end
-
-    vim.api.nvim_buf_set_lines(0, 0, #lines, false, lines)
+    vim.api.nvim_buf_set_lines(0, 0, #lines + 1, true, lines)
 end, {
     desc = "Sort Brewfiles properly by category on write.",
     pattern = { "brewfile", "brewfile.*" },
