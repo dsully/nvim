a70a05321fb1d7d0406596bf6e2b7706a3937a59
Dan Sully <dsully@users.noreply.github.com>
2024-05-05 20:24:27 -0700

    global: Add debug function

--- lua/helpers/globals.lua
+++ lua/helpers/globals.lua
@@ -1,4 +1,17 @@
 --
+--- Global debug function
+---@vararg any anything to debug
+_G.dbg = function(...)
+    local objects = {}
+
+    for _, v in pairs({ ... }) do
+        table.insert(objects, v ~= nil and vim.inspect(v) or "nil")
+    end
+
+    vim.notify(table.concat(objects, "\n"))
+end
+
+--
 --- Global telescope loader.
 ---
 ---@param command string The telescope command to run.
