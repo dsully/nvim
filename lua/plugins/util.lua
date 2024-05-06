135e1adef6c2bc1e5e6103220665f36b1611dafc
Dan Sully <dan+github@sully.org>
2024-05-05 20:20:13 -0700

    resession: Update hooks
    Update statuscol to match.

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -11,6 +11,41 @@ return {
     },
     {
         "stevearc/resession.nvim",
+        config = function(_, opts)
+            local e = require("helpers.event")
+            local resession = require("resession")
+            local lazy_open = false
+
+            resession.setup(opts)
+
+            resession.add_hook("post_load", function()
+                -- Fixes lazy freaking out when a session is loaded and there are auto-installs being run.
+                if lazy_open then
+                    require("lazy.view").show()
+                    lazy_open = false
+                end
+
+                vim.cmd.doautoall(e.BufReadPost)
+                vim.cmd.doautoall(e.SessionLoadPost)
+            end)
+
+            resession.add_hook("pre_load", function()
+                local view = require("lazy.view")
+
+                if view.view then
+                    lazy_open = true
+
+                    if view.view:buf_valid() then
+                        vim.api.nvim_buf_delete(view.view.buf, { force = true })
+                    end
+                    view.view:close({ wipe = true })
+                else
+                    lazy_open = false
+                end
+
+                require("lspconfig")
+            end)
+        end,
         init = function()
             local e = require("helpers.event")
 
@@ -23,8 +58,6 @@ return {
 
             vim.api.nvim_create_user_command("SessionLoad", function()
                 require("resession").load(session_name(), { silence_errors = false })
-
-                vim.cmd.doautoall(e.BufReadPost)
             end, { desc = "Session Load" })
 
             e.on(e.VimLeavePre, function()
