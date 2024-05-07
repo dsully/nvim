8d659b76b44ed98c31ad0cdb1f685256831d875a
Dan Sully <dan+github@sully.org>
2024-05-06 19:36:22 -0700

    help: Render boxes in popup.

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -27,26 +27,10 @@ e.on(e.BufWinEnter, function(event)
         return
     end
 
-    local filetype = vim.bo[event.buf].filetype
-    local file_path = event.match
-
-    if file_path:match("/doc/") ~= nil then
-        --
-        if filetype == "help" or filetype == "markdown" then
-            local help_win = vim.api.nvim_get_current_win()
-
-            require("helpers.float").open({
-                anchor = "E",
-                filetype = filetype,
-                lines = vim.api.nvim_buf_get_lines(event.buf, 0, -1, false),
-            })
-
-            -- Close the initial help split window.
-            vim.api.nvim_win_close(help_win, false)
-        end
-    end
+    require("helpers.help").popup(event)
 end, {
     desc = "Open Help in a floating window.",
+    group = e.group("filetype.help"),
 })
 
 e.on(e.FileType, function()
