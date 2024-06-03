6c7d8834e9e2ce18b20029821d6ccb48b1941452
Dan Sully <dan+github@sully.org>
2024-06-03 13:46:00 -0700

    Remove markdown plugin.
    Update key map descriptions.
    
    Try fixing Esc not registering with ttimeoutlen
    
    Change "Yank All Lines" keymap to not conflict with Copilot Chat.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -41,6 +41,8 @@ vim.opt.shortmess:append({ W = true, I = true, c = true, q = true })
 vim.opt.softtabstop = 4
 vim.opt.termguicolors = true
 vim.opt.textwidth = 160
+vim.opt.ttimeout = true
+vim.opt.ttimeoutlen = 0
 vim.opt.updatetime = 100 -- milliseconds to write the swap file.
 vim.opt.wildignore:append({ ".DS_Store" })
 
