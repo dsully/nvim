d4f602c0bd91ad0cd5741e883a997299619e11f6
Dan Sully <dsully@users.noreply.github.com>
2024-06-17 15:38:43 -0700

    formatting: Update for conform breaking change.

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -6,7 +6,7 @@ return {
         {
             "<space>f",
             function()
-                require("conform").format({ async = true, lsp_fallback = true, timeout_ms = 3000 })
+                require("conform").format({ async = true, lsp_format = "first", timeout_ms = 3000 })
             end,
             desc = "󰛗 Format Buffer",
         },
@@ -29,7 +29,7 @@ return {
                     return {}
                 end
 
-                return { timeout_ms = 500, lsp_fallback = true }
+                return { timeout_ms = 500, lsp_format = "first" }
             end,
             formatters = {
                 caddy = {
