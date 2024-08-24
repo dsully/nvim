69621c760a8e0b8b4950476a49ecf7c7c35988a6
Dan Sully <dsully@users.noreply.github.com>
2024-08-24 12:40:34 -0700

    markdown: Add checkbox keymap

--- lua/plugins/filetypes.lua
+++ lua/plugins/filetypes.lua
@@ -62,12 +62,32 @@ return {
     },
     {
         "MeanderingProgrammer/render-markdown.nvim",
+        cmd = "RenderMarkdown",
+        ft = { "markdown", "vimwiki" },
         keys = {
+            {
+                "<leader>x",
+                function()
+                    local char = "x"
+                    local current_line = vim.api.nvim_get_current_line()
+
+                    local _, _, checkbox_state = string.find(current_line, "%[([ " .. char .. string.upper(char) .. "])%]")
+
+                    if checkbox_state then
+                        local new_state = checkbox_state == " " and char or " "
+                        local new_line = string.gsub(current_line, "%[.-%]", "[" .. new_state .. "]")
+
+                        vim.api.nvim_set_current_line(new_line)
+                    end
+                end,
+                desc = "Toggle checkbox",
+                { noremap = true, silent = true },
+            },
             -- stylua: ignore
             { "<leader>um", function() vim.cmd.RenderMarkdown("toggle") end, desc = "Render Markdown", },
         },
-        ft = { "markdown", "vimwiki" },
         opts = {
+            enabled = false, -- Off by default.
             file_types = { "markdown", "vimwiki" },
             code = {
                 sign = false,
