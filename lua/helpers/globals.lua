29958185b5b7371fa24e26cd574e7a8f96fe0be3
Dan Sully <dsully@users.noreply.github.com>
2024-09-15 08:14:44 -0700

    globals: Update debug functions

--- lua/helpers/globals.lua
+++ lua/helpers/globals.lua
@@ -3,21 +3,90 @@ _G.defaults = require("config.defaults")
 _G.ev = require("helpers.event")
 _G.keys = require("helpers.keys")
 
---- Global debug function
----@param ... any
-_G.dbg = function(...)
-    local info = debug.getinfo(2, "S")
-    local source = info.source:sub(2)
+---@return string
+local location = function()
+    ---@type debuginfo
+    local me = debug.getinfo(1, "S")
+    local level = 2
 
-    local args = { ... }
+    ---@type debuginfo
+    local info = debug.getinfo(level, "S")
 
-    if vim.islist(args) and vim.tbl_count(args) <= 1 then
-        args = args[1]
+    while info and (info.source == me.source or info.source == "@" .. vim.env.MYVIMRC or info.what ~= "Lua") do
+        level = level + 1
+        info = debug.getinfo(level, "S")
     end
 
-    local msg = "Source: " .. source("\n\n") .. vim.split(vim.inspect(vim.deepcopy(args)), "\n")
+    info = info or me
 
-    require("helpers.float").open({ filetype = "lua", lines = msg, window = { width = 0.8 } })
+    local source = info.source:sub(2)
+
+    source = vim.uv.fs_realpath(source) or source
+
+    return source .. ":" .. info.linedefined
+end
+
+---@param value any
+---@param opts? {location:string, bt?:boolean}
+local _dump = function(value, opts)
+    opts = opts or {}
+    opts.location = opts.location or location()
+
+    if vim.in_fast_event() then
+        return vim.schedule(function()
+            M._dump(value, opts)
+        end)
+    end
+    opts.location = vim.fn.fnamemodify(opts.location, ":~:.")
+
+    local msg = vim.inspect(value)
+
+    if opts.bt then
+        msg = msg .. "\n" .. debug.traceback("", 2)
+    end
+
+    vim.notify(msg, vim.log.levels.INFO, {
+        title = "Debug: " .. opts.location,
+        on_open = function(win)
+            vim.wo[win].conceallevel = 3
+            vim.wo[win].concealcursor = ""
+            vim.wo[win].spell = false
+
+            local buf = vim.api.nvim_win_get_buf(win)
+
+            if not pcall(vim.treesitter.start, buf, "lua") then
+                vim.bo[buf].filetype = "lua"
+            end
+        end,
+    })
+end
+
+--- Global debug function
+_G.dbg = function(...)
+    ---@type any
+    local value = { ... }
+
+    if vim.tbl_isempty(value) then
+        value = nil
+    else
+        value = vim.islist(value) and vim.tbl_count(value) <= 1 and value[1] or value
+    end
+
+    _dump(value)
+end
+
+--- Global backtrace function
+_G.bt = function(...)
+    ---@type any
+    local value = { ... }
+
+    if vim.tbl_isempty(value) then
+        value = nil
+    else
+        value = vim.islist(value) and vim.tbl_count(value) <= 1 and value[1] or value
+    end
+
+    _dump(value, { bt = true })
 end
 
 --- Create a namespace.
