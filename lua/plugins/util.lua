1f2ea559382a50d423985161e03556c8d0217f38
Dan Sully <dsully@users.noreply.github.com>
2024-09-17 05:44:19 -0700

    recession: See if we can remove the loading hooks

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -3,40 +3,6 @@ return {
     { "MunifTanjim/nui.nvim" },
     {
         "stevearc/resession.nvim",
-        config = function(_, opts)
-            local resession = require("resession")
-            local lazy_open = false
-
-            resession.setup(opts)
-
-            resession.add_hook("post_load", function()
-                -- Fixes lazy freaking out when a session is loaded and there are auto-installs being run.
-                if lazy_open then
-                    require("lazy.view").show()
-                    lazy_open = false
-                end
-
-                vim.cmd.doautoall(ev.BufReadPost)
-                vim.cmd.doautoall(ev.SessionLoadPost)
-            end)
-
-            resession.add_hook("pre_load", function()
-                local view = require("lazy.view")
-
-                if view.view then
-                    lazy_open = true
-
-                    if view.view:buf_valid() then
-                        vim.api.nvim_buf_delete(view.view.buf, { force = true })
-                    end
-                    view.view:close({ wipe = true })
-                else
-                    lazy_open = false
-                end
-
-                require("lspconfig")
-            end)
-        end,
         init = function()
             local function session_name()
                 local cwd = tostring(vim.uv.cwd())
