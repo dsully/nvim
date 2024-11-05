2bde486e8cc7fdab5195156fa35b9620cc397f15
Dan Sully <dan+github@sully.org>
2024-11-04 20:48:24 -0800

    dressing: Tweak loader

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -8,9 +8,15 @@ return {
         "stevearc/dressing.nvim",
         init = function()
             ---@diagnostic disable-next-line: duplicate-set-field
-            vim.ui.select = function(...)
-                require("lazy").load({ plugins = { "dressing.nvim" } })
-                return vim.ui.select(...)
+            vim.ui.select = function(items, opts, on_choice)
+                if not package.loaded["dressing.nvim"] then
+                    require("lazy").load({ plugins = { "dressing.nvim" } })
+                end
+
+                -- Don't show the picker if there's nothing to pick.
+                if #items > 0 then
+                    return vim.ui.select(items, opts, on_choice)
+                end
             end
         end,
         opts = {
