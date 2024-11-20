40016517f1ccccd702b191f471eac07e613cc1c3
Dan Sully <dan+github@sully.org>
2024-11-20 10:51:02 -0800

    notify: Apply LazyVim fix for snacks/noice

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -170,6 +170,13 @@ return {
     },
     {
         "folke/snacks.nvim",
+        config = function(_, opts)
+            local notify = vim.notify
+            require("snacks").setup(opts)
+            -- HACK: restore vim.notify after snacks setup and let noice.nvim take over
+            -- this is needed to have early notifications show up in noice history
+            vim.notify = notify
+        end,
         priority = 1000,
         lazy = false,
         ---@type snacks.Config
