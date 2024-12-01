1ebd70da22a0e4746fca16ab0228b9dd02a45e0c
Dan Sully <dan+github@sully.org>
2024-12-01 15:12:38 -0800

    snacks: Add profiler + keymaps

--- init.lua
+++ init.lua
@@ -4,4 +4,16 @@ vim.loader.enable()
 ---@diagnostic disable-next-line: duplicate-set-field
 vim.deprecate = function() end
 
+if vim.env.PROF or vim.env.PROFILE or vim.env.NVIM_PROFILE then
+    local snacks = vim.fn.stdpath("data") .. "/lazy/snacks.nvim"
+
+    vim.opt.rtp:append(snacks)
+
+    require("snacks.profiler").startup({
+        startup = {
+            event = "VeryLazy", -- Stop profiler on this event. Defaults to `VimEnter`
+        },
+    })
+end
+
 require("config")
