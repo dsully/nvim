11b349bfa53a54f3dd6e0559c6f90856f69483ad
Dan Sully <dan+github@sully.org>
2024-12-02 07:59:59 -0800

    chezmoi: Defer loading managed file list

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -106,20 +106,24 @@ return {
             })
 
             -- Watch chezmoi files for changes in the target-path, and add them.
-            vim.schedule(function()
-                local ok, targets = pcall(require("chezmoi.commands").list, { args = { "--include", "files", "--path-style", "absolute" } })
+            ev.on(ev.BufReadPost, function()
+                vim.defer_fn(function()
+                    local ok, targets = pcall(require("chezmoi.commands").list, { args = { "--include", "files", "--path-style", "absolute" } })
 
-                if ok then
-                    ev.on(ev.BufWritePost, function(args)
-                        notify.info("chezmoi: Adding changes to: " .. args.file)
+                    if ok then
+                        ev.on(ev.BufWritePost, function(args)
+                            notify.info("chezmoi: Adding changes to: " .. args.file)
 
-                        vim.system({ "chezmoi", "add", args.file }, { text = true }):wait()
-                    end, {
-                        desc = "Apply chezmoi changes via 'chezmoi edit'",
-                        pattern = targets,
-                    })
-                end
-            end)
+                            vim.system({ "chezmoi", "add", args.file }, { text = true }):wait()
+                        end, {
+                            desc = "Apply chezmoi changes via 'chezmoi edit'",
+                            pattern = targets,
+                        })
+                    end
+                end, 500)
+            end, {
+                once = true,
+            })
 
             vim.api.nvim_create_user_command("ChezmoiFzf", function()
                 require("fzf-lua").fzf_exec(require("chezmoi.commands").list({}), {
