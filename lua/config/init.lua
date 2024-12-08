d6b3f125982f82fa6f99e67d08262ee4c1f287d2
Dan Sully <dsully@users.noreply.github.com>
2024-12-08 08:39:53 -0800

    filetypes: Clean up and merge into options.lua

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -3,19 +3,17 @@ require("config.options")
 
 vim.cmd.colorscheme(vim.g.colorscheme)
 
--- perf: Defer loading of filetype and autocommands if no files were passed.
+-- perf: Defer loading of autocommands if no files were passed.
 if vim.fn.argc(-1) == 0 then
     --
     ev.on(ev.User, function()
         require("config.autocommands")
-        require("config.filetype")
     end, {
         pattern = "VeryLazy",
         once = true,
     })
 else
     require("config.autocommands")
-    require("config.filetype")
 end
 
 require("config.lazy").setup()
