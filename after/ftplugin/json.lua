00138f062ef9212ff748679c98772e6e48e47ea1
Dan Sully <dan+github@sully.org>
2024-12-12 11:04:40 -0800

    keymap updates

--- after/ftplugin/json.lua
+++ after/ftplugin/json.lua
@@ -1,8 +1,9 @@
 -- https://medium.com/scoro-engineering/5-smart-mini-snippets-for-making-text-editing-more-fun-in-neovim-b55ffb96325a
-vim.keymap.set("n", "o", function()
+
+keys.map("o", function()
     if string.find(vim.api.nvim_get_current_line(), "[^,{[]$") then
         return "A,<cr>"
     end
 
     return "o"
-end, { buffer = true, expr = true })
+end, "Split JSON lines", { buffer = true, expr = true })
--- lua/plugins/filetypes.lua
+++ lua/plugins/filetypes.lua
@@ -34,10 +34,10 @@ return {
 
             package.setup()
 
-            vim.keymap.set("n", "<leader>nu", package.update, { desc = "Package: Update package on line" })
-            vim.keymap.set("n", "<leader>nd", package.delete, { desc = "Package: Delete package on line" })
-            vim.keymap.set("n", "<leader>ni", package.install, { desc = "Package: Install new package" })
-            vim.keymap.set("n", "<leader>nv", package.change_version, { desc = "Package: Change version of package on line" })
+            keys.map("<leader>nu", package.update, "Package: Update package on line")
+            keys.map("<leader>nd", package.delete, "Package: Delete package on line")
+            keys.map("<leader>ni", package.install, "Package: Install new package")
+            keys.map("<leader>nv", package.change_version, "Package: Change version of package on line")
         end,
         event = "BufRead package.json",
     },
