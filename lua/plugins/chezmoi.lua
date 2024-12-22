15a49a5ecd3b5715361983362c12a8450be4b3d0
Dan Sully <dan+github@sully.org>
2024-12-22 14:17:58 -0800

    chezmoi: Defer loading

--- lua/plugins/chezmoi.lua
+++ lua/plugins/chezmoi.lua
@@ -4,50 +4,54 @@ return {
         "ChezmoiEdit",
         "ChezmoiList",
     },
-    init = function()
-        -- Watch chezmoi files for changes in the source-path, and apply them.
-        ev.on(ev.BufWritePost, function(args)
-            if args.file and args.file:match(".tmpl") then
-                return
-            end
-
-            vim.schedule(require("chezmoi.commands.__edit").watch)
-        end, {
-            pattern = { vim.env.XDG_DATA_HOME .. "/chezmoi/home/*" },
-        })
-
-        -- Watch chezmoi files for changes in the target-path, and add them.
-        ev.on(ev.BufReadPost, function()
-            vim.defer_fn(function()
-                local ok, targets = pcall(require("chezmoi.commands").list, { args = { "--include", "files", "--path-style", "absolute" } })
-
-                if ok then
-                    ev.on(ev.BufWritePost, function(args)
-                        notify.info("chezmoi: Adding changes to: " .. args.file)
-
-                        vim.system({ "chezmoi", "add", args.file }, { text = true }):wait()
-                    end, {
-                        desc = "Apply chezmoi changes via 'chezmoi edit'",
-                        pattern = targets,
-                    })
+    config = function()
+        --
+        vim.schedule(function()
+            --
+            -- Watch chezmoi files for changes in the source-path, and apply them.
+            ev.on(ev.BufWritePost, function(args)
+                if args.file and args.file:match(".tmpl") then
+                    return
                 end
-            end, 500)
-        end, {
-            once = true,
-        })
 
-        vim.api.nvim_create_user_command("ChezmoiFzf", function()
-            require("fzf-lua").fzf_exec(require("chezmoi.commands").list({}), {
-                actions = {
-                    ["default"] = function(selected, _opts)
-                        require("chezmoi.commands").edit({
-                            targets = { vim.env.HOME .. selected[1] },
-                            args = { "--watch" },
-                        })
-                    end,
-                },
+                vim.schedule(require("chezmoi.commands.__edit").watch)
+            end, {
+                pattern = { vim.env.XDG_DATA_HOME .. "/chezmoi/home/*" },
             })
-        end, { desc = "Chezmoi Fzf" })
+
+            -- Watch chezmoi files for changes in the target-path, and add them.
+            ev.on(ev.BufReadPost, function()
+                vim.defer_fn(function()
+                    local ok, targets = pcall(require("chezmoi.commands").list, { args = { "--include", "files", "--path-style", "absolute" } })
+
+                    if ok then
+                        ev.on(ev.BufWritePost, function(args)
+                            notify.info("chezmoi: Adding changes to: " .. args.file)
+
+                            vim.system({ "chezmoi", "add", args.file }, { text = true }):wait()
+                        end, {
+                            desc = "Apply chezmoi changes via 'chezmoi edit'",
+                            pattern = targets,
+                        })
+                    end
+                end, 2000)
+            end, {
+                once = true,
+            })
+
+            vim.api.nvim_create_user_command("ChezmoiFzf", function()
+                require("fzf-lua").fzf_exec(require("chezmoi.commands").list({}), {
+                    actions = {
+                        ["default"] = function(selected, _opts)
+                            require("chezmoi.commands").edit({
+                                targets = { vim.env.HOME .. selected[1] },
+                                args = { "--watch" },
+                            })
+                        end,
+                    },
+                })
+            end, { desc = "Chezmoi Fzf" })
+        end)
     end,
     opts = {},
 }
