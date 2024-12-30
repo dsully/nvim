a654d866b2406b403da40fa1f4e479e27ec7d47a
Dan Sully <dsully@users.noreply.github.com>
2024-12-30 06:15:06 -0800

    perf: Defer shada loading

--- init.lua
+++ init.lua
@@ -1,6 +1,22 @@
 -- https://github.com/neovim/neovim/pull/24044
 vim.loader.enable()
 
+-- Loading shada is SLOW, so we're going to load it manually, after UI-enter so it doesn't block startup.
+local shada = vim.o.shada
+
+vim.o.shada = ""
+
+vim.api.nvim_create_autocmd("User", {
+    callback = function()
+        vim.o.shada = shada
+        pcall(vim.cmd.rshada, { bang = true })
+    end,
+    pattern = "VeryLazy",
+})
+
+-- Work around: https://github.com/neovim/neovim/issues/31675
+vim.hl = vim.highlight
+
 ---@diagnostic disable-next-line: duplicate-set-field
 vim.deprecate = function() end
 
