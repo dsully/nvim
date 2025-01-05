7d07a401fb6f1e2ebbc56015708d4c48cd9eb496
Dan Sully <dan+github@sully.org>
2025-01-05 15:36:07 -0800

    supermaven: Extend blink and noice

--- lua/plugins/ai/supermaven.lua
+++ lua/plugins/ai/supermaven.lua
@@ -1,23 +1,76 @@
 return {
-    "supermaven-inc/supermaven-nvim",
-    cmd = {
-        "SupermavenUseFree",
-        "SupermavenUsePro",
-    },
-    config = function(_, opts)
-        --
-        ev.on_load("blink.cmp", function()
-            vim.schedule(function()
-                require("supermaven-nvim").setup(opts)
-            end)
-        end)
-    end,
-    opts = {
-        keymaps = {
-            -- Handled by blink
-            accept_suggestion = nil,
+    {
+        "supermaven-inc/supermaven-nvim",
+        cmd = {
+            "SupermavenUseFree",
+            "SupermavenUsePro",
         },
-        disable_inline_completion = true,
-        ignore_filetypes = defaults.ignored.file_types,
+        config = function(_, opts)
+            --
+            ev.on_load("blink.cmp", function()
+                vim.schedule(function()
+                    require("blink-compat")
+                    require("supermaven-nvim").setup(opts)
+                end)
+            end)
+        end,
+        event = ev.InsertEnter,
+        opts = {
+            keymaps = {
+                -- Handled by blink
+                accept_suggestion = nil,
+            },
+            disable_inline_completion = true,
+            ignore_filetypes = defaults.ignored.file_types,
+        },
+        { "saghen/blink.compat" },
+        {
+            "Saghen/blink.cmp",
+            dependencies = { "blink.compat", "supermaven-nvim" },
+            optional = true,
+            opts = {
+                sources = {
+                    default = { "supermaven" },
+                    providers = {
+                        supermaven = {
+                            name = "supermaven",
+                            async = true,
+                            module = "blink.compat.source",
+                            score_offset = 100,
+                            transform_items = function(_, items)
+                                local kind = require("blink.cmp.types").CompletionItemKind
+                                local kind_idx = #kind + 1
+
+                                kind[kind_idx] = "Supermaven"
+
+                                for _, item in ipairs(items) do
+                                    item.kind = kind_idx
+                                end
+
+                                return items
+                            end,
+                        },
+                    },
+                },
+            },
+        },
+        {
+            "folke/noice.nvim",
+            optional = true,
+            opts = {
+                routes = {
+                    {
+                        filter = {
+                            event = "msg_show",
+                            any = {
+                                { find = "Starting Supermaven" },
+                                { find = "Supermaven Free Tier" },
+                            },
+                        },
+                        skip = true,
+                    },
+                },
+            },
+        },
     },
 }
