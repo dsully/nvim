1b718e0d2165eb66c06c31525bc8e09d28aab58b
Dan Sully <dsully@users.noreply.github.com>
2025-01-06 14:54:48 -0800

    blink: Move Rust types to _meta

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,14 +1,4 @@
--- CompletionItem.data that rust-analyzer returns.
----@class RustCompletionImport
----@field full_import_path string
----@field imported_name string
-
----@class RustCompletionResolveData
----@field imports RustCompletionImport[]
----@field position lsp.TextDocumentPositionParams
-
----@alias RustData RustCompletionResolveData | nil
-
+---@return boolean
 local function has_words_before()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
