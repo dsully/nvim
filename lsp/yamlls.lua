7abfff49e0b2b439f7a94ae3afa70fbcebedaf49
Dan Sully <dan+github@sully.org>
2025-01-09 11:06:36 -0800

    yamlls: Fix command calling

--- lsp/yamlls.lua
+++ lsp/yamlls.lua
@@ -1,16 +1,15 @@
+---@type vim.lsp.Config
 return {
     cmd = { "yaml-language-server", "--stdio" },
     commands = {
-        YAMLSchema = {
-            function()
-                local schema = require("schema-companion").get_buf_schema(vim.api.nvim_get_current_buf())
+        --- @type fun(command: lsp.Command, ctx: table)>
+        YAMLSchema = function()
+            local schema = require("schema-companion").get_buf_schema(vim.api.nvim_get_current_buf())
 
-                if schema.result[1].name ~= "none" then
-                    notify.info(schema.result[1].name)
-                end
-            end,
-            desc = "Show YAML schema",
-        },
+            if schema.result[1].name ~= "none" then
+                notify.info(schema.result[1].name)
+            end
+        end,
     },
     filetypes = { "yaml" },
     handlers = {
@@ -26,7 +25,6 @@ return {
     --- @param client vim.lsp.Client
     on_init = function(client)
         client:notify("yaml/supportSchemaSelection", { {} })
-        return true
     end,
     on_new_config = function(config)
         config.settings = vim.tbl_deep_extend("force", config.settings, {
