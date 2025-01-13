f07ad0a0d4dfd8d1a576bf848c5c35f6c3a71eb1
Dan Sully <dan+github@sully.org>
2025-01-12 16:26:30 -0800

    mini: Clean up mini.ai config

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -14,43 +14,91 @@ return {
         config = function(_, opts)
             --
             vim.schedule(function()
-                require("mini.ai").setup(opts)
+                local ai = require("mini.ai")
+
+                ai.setup({
+                    custom_textobjects = {
+                        -- 'vaF' to select around function definition.
+                        -- 'diF' to delete inside function definition.
+                        c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),
+                        f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
+                        l = require("mini.extra").gen_ai_spec.line(),
+
+                        o = ai.gen_spec.treesitter({ -- code block
+                            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+                            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+                        }),
+                    },
+                    n_lines = 2000,
+                })
 
                 ev.on_load("which-key.nvim", function()
-                    require("helpers.mini").ai_whichkey(opts)
+                    local objects = {
+                        { " ", desc = "whitespace" },
+                        { '"', desc = '" string' },
+                        { "'", desc = "' string" },
+                        { "(", desc = "() block" },
+                        { ")", desc = "() block with ws" },
+                        { "<", desc = "<> block" },
+                        { ">", desc = "<> block with ws" },
+                        { "?", desc = "user prompt" },
+                        { "U", desc = "use/call without dot" },
+                        { "[", desc = "[] block" },
+                        { "]", desc = "[] block with ws" },
+                        { "_", desc = "underscore" },
+                        { "`", desc = "` string" },
+                        { "a", desc = "argument" },
+                        { "b", desc = ")]} block" },
+                        { "c", desc = "class" },
+                        { "d", desc = "digit(s)" },
+                        { "e", desc = "CamelCase / snake_case" },
+                        { "f", desc = "function" },
+                        { "g", desc = "entire file" },
+                        { "i", desc = "indent" },
+                        { "o", desc = "block, conditional, loop" },
+                        { "q", desc = "quote `\"'" },
+                        { "t", desc = "tag" },
+                        { "u", desc = "use/call" },
+                        { "{", desc = "{} block" },
+                        { "}", desc = "{} with ws" },
+                    }
+
+                    ---@type wk.Spec[]
+                    local ret = { mode = { "o", "v" } }
+
+                    ---@type table<string, string>
+                    local mappings = vim.tbl_extend("force", {}, {
+                        around = "a",
+                        inside = "i",
+                        around_next = "an",
+                        inside_next = "in",
+                        around_last = "al",
+                        inside_last = "il",
+                    }, opts.mappings or {})
+
+                    mappings.goto_left = nil
+                    mappings.goto_right = nil
+
+                    for name, prefix in pairs(mappings) do
+                        name = name:gsub("^around_", ""):gsub("^inside_", "")
+                        ret[#ret + 1] = { prefix, group = name }
+
+                        for _, obj in ipairs(objects) do
+                            local desc = obj.desc
+
+                            if prefix:sub(1, 1) == "i" then
+                                desc = desc:gsub(" with ws", "")
+                            end
+
+                            ret[#ret + 1] = { prefix .. obj[1], desc = desc }
+                        end
+                    end
+
+                    require("which-key").add(ret, { notify = false })
                 end)
             end)
         end,
         event = ev.LazyFile,
-        opts = function()
-            local ai = require("mini.ai")
-
-            return {
-                n_lines = 2000,
-                custom_textobjects = {
-                    o = ai.gen_spec.treesitter({ -- code block
-                        a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-                        i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-                    }),
-
-                    -- 'vaF' to select around function definition.
-                    -- 'diF' to delete inside function definition.
-                    f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
-                    c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
-
-                    t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
-                    d = { "%f[%d]%d+" }, -- digits
-                    e = { -- Word with case
-                        { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
-                        "^().*()$",
-                    },
-                    g = require("helpers.mini").ai_buffer, -- buffer
-
-                    u = ai.gen_spec.function_call(), -- u for "Usage"
-                    U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
-                },
-            }
-        end,
         virtual = true,
     },
     {
