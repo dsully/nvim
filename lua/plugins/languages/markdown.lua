f4abf293a6009e2645c4f9b867b232d9d5ce0f12
Dan Sully <dan+github@sully.org>
2025-01-16 12:07:54 -0800

    markdown: Dial already has checkbox toggles.

--- lua/plugins/languages/markdown.lua
+++ lua/plugins/languages/markdown.lua
@@ -43,23 +43,8 @@ return {
             },
             on = {
                 ---@param buf integer
-                attach = function(buf)
+                attach = function()
                     --
-                    -- Toggle check boxes.
-                    keys.bmap("<space>tc", function()
-                        local char = defaults.icons.misc.check
-                        local current_line = vim.api.nvim_get_current_line()
-
-                        local _, _, current_state = string.find(current_line, "%[([ " .. char .. string.upper(char) .. "])%]")
-
-                        if current_state then
-                            local new_state = current_state == " " and char or " "
-                            local new_line = string.gsub(current_line, "%[.-%]", "[" .. new_state .. "]")
-
-                            vim.api.nvim_set_current_line(new_line)
-                        end
-                    end, "Checkbox", buf, "n", { noremap = true, silent = true })
-
                     require("snacks")
                         .toggle({
                             name = "Markdown",
@@ -79,6 +64,14 @@ return {
                 end,
             },
             overrides = {
+                buftype = {
+                    nofile = {
+                        anti_conceal = {
+                            enabled = false,
+                        },
+                        render_modes = true,
+                    },
+                },
                 filetype = {
                     codecompanion = {
                         anti_conceal = {
