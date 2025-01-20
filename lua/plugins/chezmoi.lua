1f9582ae9f3c10316c638c07d3992e4050519f86
Dan Sully <dsully@users.noreply.github.com>
2025-01-19 18:21:40 -0800

    chezmoi: Update mini.icons
    Use Snacks picker

--- lua/plugins/chezmoi.lua
+++ lua/plugins/chezmoi.lua
@@ -1,58 +1,103 @@
 ---@type LazySpec
 return {
-    "xvzc/chezmoi.nvim",
-    cmd = {
-        "ChezmoiEdit",
-        "ChezmoiList",
-    },
-    config = function()
-        --
-        vim.schedule(function()
+    {
+        "xvzc/chezmoi.nvim",
+        cmd = {
+            "ChezmoiEdit",
+            "ChezmoiList",
+        },
+        config = function()
             --
-            -- Watch chezmoi files for changes in the source-path, and apply them.
-            ev.on(ev.BufWritePost, function(args)
-                if args.file and args.file:match(".tmpl") then
-                    return
-                end
+            vim.schedule(function()
+                --
+                -- Watch chezmoi files for changes in the source-path, and apply them.
+                ev.on(ev.BufWritePost, function(args)
+                    if args.file and args.file:match(".tmpl") then
+                        return
+                    end
 
-                vim.schedule(require("chezmoi.commands.__edit").watch)
-            end, {
-                pattern = { vim.env.XDG_DATA_HOME .. "/chezmoi/home/*" },
-            })
+                    vim.schedule(require("chezmoi.commands.__edit").watch)
+                end, {
+                    pattern = { vim.env.XDG_DATA_HOME .. "/chezmoi/home/*" },
+                })
 
-            -- Watch chezmoi files for changes in the target-path, and add them.
-            ev.on(ev.BufReadPost, function()
-                vim.defer_fn(function()
-                    local ok, targets = pcall(require("chezmoi.commands").list, { args = { "--include", "files", "--path-style", "absolute" } })
+                -- Watch chezmoi files for changes in the target-path, and add them.
+                ev.on(ev.BufReadPost, function()
+                    vim.defer_fn(function()
+                        local ok, targets = pcall(require("chezmoi.commands").list, { args = { "--include", "files", "--path-style", "absolute" } })
 
-                    if ok then
-                        ev.on(ev.BufWritePost, function(args)
-                            notify.info("chezmoi: Adding changes to: " .. args.file)
+                        if ok then
+                            ev.on(ev.BufWritePost, function(args)
+                                notify.info("chezmoi: Adding changes to: " .. args.file)
 
-                            vim.system({ "chezmoi", "add", args.file }, { text = true }):wait()
-                        end, {
-                            desc = "Apply chezmoi changes via 'chezmoi edit'",
-                            pattern = targets,
+                                vim.system({ "chezmoi", "add", args.file }, { text = true }):wait()
+                            end, {
+                                desc = "Apply chezmoi changes via 'chezmoi edit'",
+                                pattern = targets,
+                            })
+                        end
+                    end, 2000)
+                end, {
+                    once = true,
+                })
+            end)
+        end,
+        keys = {
+            {
+                "<leader>fz",
+                function()
+                    local results = require("chezmoi.commands").list({
+                        args = {
+                            "--path-style",
+                            "absolute",
+                            "--include",
+                            "files",
+                            "--exclude",
+                            "externals",
+                        },
+                    })
+
+                    local items = {}
+
+                    for _, file in ipairs(results) do
+                        table.insert(items, {
+                            text = file,
+                            file = file,
                         })
                     end
-                end, 2000)
-            end, {
-                once = true,
-            })
 
-            vim.api.nvim_create_user_command("ChezmoiFzf", function()
-                require("fzf-lua").fzf_exec(require("chezmoi.commands").list({}), {
-                    actions = {
-                        ["default"] = function(selected, _opts)
+                    ---@type snacks.picker.Config
+                    Snacks.picker.pick({
+                        confirm = function(picker, item)
+                            picker:close()
                             require("chezmoi.commands").edit({
-                                targets = { vim.env.HOME .. selected[1] },
+                                targets = { item.text },
                                 args = { "--watch" },
                             })
                         end,
-                    },
-                })
-            end, { desc = "Chezmoi Fzf" })
-        end)
-    end,
-    opts = {},
+                        items = items,
+                    })
+                end,
+                desc = "Chezmoi",
+            },
+        },
+        opts = {},
+    },
+    {
+        "echasnovski/mini.icons",
+        optional = true,
+        opts = {
+            file = {
+                [".chezmoiignore"] = { glyph = "", hl = "MiniIconsGrey" },
+                [".chezmoiremove"] = { glyph = "", hl = "MiniIconsGrey" },
+                [".chezmoiroot"] = { glyph = "", hl = "MiniIconsGrey" },
+                [".chezmoiversion"] = { glyph = "", hl = "MiniIconsGrey" },
+                ["fish.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+                ["json.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+                ["sh.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+                ["toml.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+                ["yaml.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+            },
+        },
+    },
 }
