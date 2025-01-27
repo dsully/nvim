6883fa2323bdce7014e1433de07e65900c7bf002
Dan Sully <dsully@users.noreply.github.com>
2025-01-27 05:48:17 -0800

    gopls: Supports static semantic tokens now

--- lsp/gopls.lua
+++ lsp/gopls.lua
@@ -5,30 +5,6 @@ return {
     init_options = {
         usePlaceholders = true,
     },
-    ---@param client vim.lsp.Client
-    on_attach = function(client)
-        -- As of v0.11.0, gopls does not send a Semantic Token legend (in a
-        -- client/registerCapability message) unless the client supports dynamic
-        -- registration. Neovim's LSP client does not support dynamic registration
-        -- for semantic tokens, so we need to declare those server_capabilities
-        -- ourselves for the time being.
-        -- Ref. https://github.com/golang/go/issues/54531
-        --
-        if not not client.server_capabilities.semanticTokensProvider then
-            local semantic = client.config.capabilities.textDocument.semanticTokens
-
-            if semantic then
-                client.server_capabilities.semanticTokensProvider = {
-                    full = true,
-                    legend = {
-                        tokenModifiers = semantic.tokenModifiers,
-                        tokenTypes = semantic.tokenTypes,
-                    },
-                    range = true,
-                }
-            end
-        end
-    end,
     root_markers = { "go.mod", "go.work" },
     settings = {
         gopls = {
