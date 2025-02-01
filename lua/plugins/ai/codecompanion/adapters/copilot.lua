a084f15cfdc14c374142582122e534a5208708b3
Dan Sully <dsully@users.noreply.github.com>
2025-02-01 13:39:31 -0800

    codecompanion: Update models

--- lua/plugins/ai/codecompanion/adapters/copilot.lua
+++ lua/plugins/ai/codecompanion/adapters/copilot.lua
@@ -2,11 +2,9 @@ return function()
     return require("codecompanion.adapters").extend("copilot", {
         schema = {
             ---@see https://github.com/copilot
-            ---@usage "gpt-4o"|"claude-3.5-sonnet"
             model = {
-                -- default = "claude-3.5-sonnet",
-                default = "o1-mini-2024-09-12",
-                choices = { "o1-mini-2024-09-12", "gpt-4o", "claude-3.5-sonnet" },
+                default = "o3-mini-2025-01-31",
+                choices = { "o3-mini-2025-01-31", "claude-3.5-sonnet" },
                 max_tokens = {
                     default = 8192,
                 },
