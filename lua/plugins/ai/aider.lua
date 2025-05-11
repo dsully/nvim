78d30b49bd9fef810879b5b64533bc47e58aa3c0
Dan Sully <dsully@users.noreply.github.com>
2025-05-11 12:11:52 -0700

    feat(plugins): try out nvim-aider

---
+++ lua/plugins/ai/aider.lua
@@ -0,0 +1,19 @@
+---@type LazySpec
+return {
+    "GeorgesAlkhouri/nvim-aider",
+    cmd = "Aider",
+    -- Example key mappings for common actions:
+    -- keys = {
+    --   { "<leader>a/", "<cmd>Aider toggle<cr>", desc = "Toggle Aider" },
+    --   { "<leader>as", "<cmd>Aider send<cr>", desc = "Send to Aider", mode = { "n", "v" } },
+    --   { "<leader>ac", "<cmd>Aider command<cr>", desc = "Aider Commands" },
+    --   { "<leader>ab", "<cmd>Aider buffer<cr>", desc = "Send Buffer" },
+    --   { "<leader>a+", "<cmd>Aider add<cr>", desc = "Add File" },
+    --   { "<leader>a-", "<cmd>Aider drop<cr>", desc = "Drop File" },
+    --   { "<leader>ar", "<cmd>Aider add readonly<cr>", desc = "Add Read-Only" },
+    --   { "<leader>aR", "<cmd>Aider reset<cr>", desc = "Reset Session" },
+    --   -- Example nvim-tree.lua integration if needed
+    --   { "<leader>a+", "<cmd>AiderTreeAddFile<cr>", desc = "Add File from Tree to Aider", ft = "NvimTree" },
+    --   { "<leader>a-", "<cmd>AiderTreeDropFile<cr>", desc = "Drop File from Tree from Aider", ft = "NvimTree" },
+    -- },
+}
