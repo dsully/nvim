e7306aa623c5605eaba1e7069dfdc13712a16c20
Dan Sully <dsully@users.noreply.github.com>
2025-05-27 18:10:23 -0700

    fix(codecompanion) anthropic adapter

--- lua/plugins/ai/codecompanion/adapters/anthropic.lua
+++ lua/plugins/ai/codecompanion/adapters/anthropic.lua
@@ -1,11 +1,26 @@
 return function()
     return require("codecompanion.adapters").extend("anthropic", {
+        ---@type CodeCompanion.Schema
         schema = {
             ---@see https://docs.anthropic.com/en/docs/about-claude/models
             model = {
                 default = "claude-opus-4-20250514",
             },
-            extended_thinking = false,
+            -- extended_output = {
+            --     condition = function(_self)
+            --         return false
+            --     end,
+            -- },
+            extended_thinking = {
+                condition = function(_self)
+                    return false
+                end,
+            },
+            thinking_budget = {
+                condition = function(_self)
+                    return false
+                end,
+            },
         },
     } --[[@as Anthropic.Adapter]])
 end
