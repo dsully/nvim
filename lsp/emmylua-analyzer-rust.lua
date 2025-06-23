f28024b6efb7fdfb94e04d7837d44882672d7a84
Dan Sully <dsully@users.noreply.github.com>
2025-06-23 05:48:24 -0700

    refactor(lsp): try emmylua-ls changes

--- lsp/emmylua-analyzer-rust.lua
+++ lsp/emmylua-analyzer-rust.lua
@@ -1,3 +1,28 @@
+local settings = {}
+
+if vim.fs.find({ ".emmyrc.json", ".luarc.json" }, { path = require("helpers.file").git_root(), type = "file" }) == false then
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = {
+                    "bit",
+                    "package",
+                    "require",
+                    "vim",
+                },
+            },
+            runtime = {
+                version = "LuaJIT",
+            },
+            workspace = {
+                library = {
+                    "$VIMRUNTIME",
+                },
+            },
+        },
+    }
+end
+
 ---@type vim.lsp.Config
 return {
     cmd = {
@@ -15,5 +40,6 @@ return {
         "stylua.toml",
         "lua/",
     },
+    settings = settings,
     single_file_support = true,
 }
