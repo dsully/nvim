7f539f9cb1fb7eabbeaf522659476730c752c2b4
dsully <dsully@users.noreply.github.com>
2025-06-29 08:48:20 -0700

    fix(gx): work around url issue. update config

--- lua/plugins/gx.lua
+++ lua/plugins/gx.lua
@@ -13,10 +13,10 @@ return {
         open_browser_app = "open",
         open_browser_args = { "--background" },
         handlers = {
-            brewfile = true,
             commit = true,
             github = true,
             go = true,
+            markdown = true,
             nix = {
                 filetype = { "nix" },
                 handle = function(mode, line, _)
@@ -32,7 +32,27 @@ return {
             },
             package_json = true, -- open dependencies from package.json
             plugin = true,
+            rust = {
+                name = "rust",
+                filename = "Cargo.toml",
+                handle = function(mode, line, _)
+                    local crate = require("gx.helper").find(line, mode, "(%w+)%s-=%s")
+
+                    if crate then
+                        return "https://crates.io/crates/" .. crate
+                    end
+                end,
+            },
             search = true,
+            -- Until https://github.com/chrishrb/gx.nvim/issues/77 is addressed.
+            url = {
+                name = "url",
+                filename = nil,
+                filetype = nil,
+                handle = function(mode, line, _)
+                    return require("gx.helper").find(line, mode, "(https?://[a-zA-Z%d_/%%%-%.~@\\+#=?&:]+)")
+                end,
+            },
         },
         handler_options = {
             search_engine = "https://kagi.com/search?q=", -- or you can pass in a custom search engine
