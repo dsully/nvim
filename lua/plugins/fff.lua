54dd09ac18b0fbf96780b724efd13db4378f2ed5
Dan Sully <dsully@users.noreply.github.com>
2025-08-17 13:07:10 -0700

    feat: try out fff.nvim

---
+++ lua/plugins/fff.lua
@@ -0,0 +1,68 @@
+---@type LazySpec[]
+return {
+    "dmtrKovalenko/fff.nvim",
+    build = "nix run .#release",
+    keys = {
+        {
+            "ff",
+            function()
+                require("fff").find_files_in_dir(nvim.file.git_root(true) or nvim.file.cwd())
+            end,
+            desc = "Find Files",
+        },
+    },
+    opts = function()
+        return {
+            base_path = nvim.file.git_root(true) or nvim.file.cwd(),
+            hl = {
+                border = "FloatBorder",
+                normal = "Normal",
+                cursor = "CursorLine",
+                matched = "IncSearch",
+                title = "Title",
+                prompt = "Question",
+                active_file = "Visual",
+                frecency = "Number",
+                debug = "Comment",
+            },
+            keymaps = {
+                close = "<Esc>",
+                select = "<CR>",
+                select_split = "<C-s>",
+                select_vsplit = "<C-v>",
+                select_tab = "<C-t>",
+                move_up = { "<Up>", "<C-p>" }, -- Multiple bindings supported
+                move_down = { "<Down>", "<C-n>" },
+                preview_scroll_up = "<C-u>",
+                preview_scroll_down = "<C-d>",
+            },
+            layout = {
+                height = 0.85,
+                width = 0.85,
+                prompt_position = "bottom",
+                preview_position = "top",
+            },
+            preview = {
+                binary_file_threshold = 1024,
+                filetypes = {
+                    log = { tail_lines = 100 },
+                    markdown = { wrap_lines = true },
+                    svg = { wrap_lines = true },
+                    text = { wrap_lines = true },
+                },
+                imagemagick_info_format_str = "%m: %wx%h, %[colorspace], %q-bit",
+                line_numbers = false,
+                max_lines = 5000,
+                max_size = 10 * 1024 * 1024,
+                show_file_info = false,
+                wrap_lines = false,
+            },
+            prompt = " ",
+            title = "",
+            ui = {
+                max_path_width = 120,
+            },
+            ui_enabled = true,
+        }
+    end,
+}
