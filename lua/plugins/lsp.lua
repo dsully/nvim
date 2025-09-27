cbaa2990e69d6dc5594b870fe3dc61c779bb2854
Dan Sully <dsully@users.noreply.github.com>
2025-09-27 09:38:33 -0700

    chore: make LazySpec a table return for lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,35 +1,36 @@
----@type LazySpec
+---@type LazySpec[]
 return {
-    "neovim/nvim-lspconfig",
-    event = ev.VeryLazy,
-    config = function()
-        local methods = vim.lsp.protocol.Methods
+    {
+        "neovim/nvim-lspconfig",
+        event = ev.VeryLazy,
+        config = function()
+            local methods = vim.lsp.protocol.Methods
 
-        vim.lsp.log.set_level(vim.lsp.log.levels.WARN)
+            vim.lsp.log.set_level(vim.lsp.log.levels.WARN)
 
-        vim.diagnostic.config({
-            float = {
-                focusable = true,
-                header = { "" },
-                severity_sort = true,
-                spacing = 2,
-                source = true,
-            },
-            severity_sort = true,
-            signs = {
-                text = {
-                    [vim.diagnostic.severity.ERROR] = defaults.icons.diagnostics.error,
-                    [vim.diagnostic.severity.WARN] = defaults.icons.diagnostics.warn,
-                    [vim.diagnostic.severity.INFO] = defaults.icons.diagnostics.info,
-                    [vim.diagnostic.severity.HINT] = defaults.icons.diagnostics.hint,
+            vim.diagnostic.config({
+                float = {
+                    focusable = true,
+                    header = { "" },
+                    severity_sort = true,
+                    spacing = 2,
+                    source = true,
                 },
-            },
-            underline = true,
-            update_in_insert = false, -- https://www.reddit.com/r/neovim/comments/pfk209/nvimlsp_too_fast/
-        } --[[@as vim.diagnostic.Opts]])
+                severity_sort = true,
+                signs = {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = defaults.icons.diagnostics.error,
+                        [vim.diagnostic.severity.WARN] = defaults.icons.diagnostics.warn,
+                        [vim.diagnostic.severity.INFO] = defaults.icons.diagnostics.info,
+                        [vim.diagnostic.severity.HINT] = defaults.icons.diagnostics.hint,
+                    },
+                },
+                underline = true,
+                update_in_insert = false, -- https://www.reddit.com/r/neovim/comments/pfk209/nvimlsp_too_fast/
+            } --[[@as vim.diagnostic.Opts]])
 
-        ev.on_load("which-key.nvim", function()
-            vim.schedule(function()
+            ev.on_load("which-key.nvim", function()
+                vim.schedule(function()
             -- stylua: ignore
             require("which-key").add({
                 { "<C-S>", vim.lsp.buf.signature_help, desc = "Signature Help", mode = "i", icon = "󰠗 " },
@@ -53,152 +54,153 @@ return {
                 { "grf", function() Snacks.rename.rename_file() end, desc = "Rename File", icon = " ", },
                 { "grr", function() Snacks.picker.lsp_references({ nowait = true }) end, desc = "References" },
             } --[[@as wk.Spec]], { notify = false })
+                end)
             end)
-        end)
 
-        -- Handle dynamic registration.
-        --
-        -- https://github.com/neovim/neovim/issues/24229
-        local register_capability = vim.lsp.handlers[methods.client_registerCapability]
+            -- Handle dynamic registration.
+            --
+            -- https://github.com/neovim/neovim/issues/24229
+            local register_capability = vim.lsp.handlers[methods.client_registerCapability]
 
-        ---@param res lsp.RegistrationParams
-        ---@param ctx lsp.HandlerContext
-        vim.lsp.handlers[methods.client_registerCapability] = function(err, res, ctx)
-            local client_id = ctx.client_id
-            local client = vim.lsp.get_client_by_id(client_id)
+            ---@param res lsp.RegistrationParams
+            ---@param ctx lsp.HandlerContext
+            vim.lsp.handlers[methods.client_registerCapability] = function(err, res, ctx)
+                local client_id = ctx.client_id
+                local client = vim.lsp.get_client_by_id(client_id)
 
-            if client ~= nil then
-                for buffer in pairs(client.attached_buffers) do
-                    --
-                    ev.emit(ev.User, {
-                        pattern = ev.LspDynamicCapability,
-                        data = { client_id = client.id, buffer = buffer },
-                    })
+                if client ~= nil then
+                    for buffer in pairs(client.attached_buffers) do
+                        --
+                        ev.emit(ev.User, {
+                            pattern = ev.LspDynamicCapability,
+                            data = { client_id = client.id, buffer = buffer },
+                        })
+                    end
                 end
+
+                return register_capability(err, res, ctx)
             end
 
-            return register_capability(err, res, ctx)
-        end
+            nvim.lsp.on_attach(nvim.lsp.validate_client)
+            nvim.lsp.on_dynamic_capability(nvim.lsp.validate_client)
+            nvim.lsp.on_dynamic_capability(function() end)
+            nvim.lsp.commands()
 
-        nvim.lsp.on_attach(nvim.lsp.validate_client)
-        nvim.lsp.on_dynamic_capability(nvim.lsp.validate_client)
-        nvim.lsp.on_dynamic_capability(function() end)
-        nvim.lsp.commands()
+            vim.lsp.on_type_formatting.enable()
 
-        vim.lsp.on_type_formatting.enable()
+            nvim.lsp.on_supports_method(methods.textDocument_documentColor, function(_, buffer)
+                vim.lsp.document_color.enable(true, buffer)
 
-        nvim.lsp.on_supports_method(methods.textDocument_documentColor, function(_, buffer)
-            vim.lsp.document_color.enable(true, buffer)
+                keys.map("grc", vim.lsp.document_color.color_presentation, "vim.lsp.document_color.color_presentation()")
 
-            keys.map("grc", vim.lsp.document_color.color_presentation, "vim.lsp.document_color.color_presentation()")
+                Snacks.toggle({
+                    name = "Color",
+                    get = function()
+                        return vim.lsp.document_color.is_enabled()
+                    end,
+                    set = function(state)
+                        vim.lsp.document_color.enable(not state)
+                    end,
+                }):map("<space>tc")
+            end)
 
-            Snacks.toggle({
-                name = "Color",
-                get = function()
-                    return vim.lsp.document_color.is_enabled()
-                end,
-                set = function(state)
-                    vim.lsp.document_color.enable(not state)
-                end,
-            }):map("<space>tc")
-        end)
+            nvim.lsp.on_supports_method(methods.textDocument_inlayHint, function()
+                vim.lsp.inlay_hint.enable(false)
+            end)
 
-        nvim.lsp.on_supports_method(methods.textDocument_inlayHint, function()
-            vim.lsp.inlay_hint.enable(false)
-        end)
+            nvim.lsp.on_supports_method(methods.textDocument_semanticTokens_full, function()
+                Snacks.toggle({
+                    name = "Semantic Tokens",
+                    get = function()
+                        return vim.lsp.semantic_tokens.is_enabled()
+                    end,
+                    set = function(state)
+                        vim.lsp.semantic_tokens.enable(not state)
+                    end,
+                }):map("<space>tS")
+            end)
 
-        nvim.lsp.on_supports_method(methods.textDocument_semanticTokens_full, function()
-            Snacks.toggle({
-                name = "Semantic Tokens",
-                get = function()
-                    return vim.lsp.semantic_tokens.is_enabled()
-                end,
-                set = function(state)
-                    vim.lsp.semantic_tokens.enable(not state)
-                end,
-            }):map("<space>tS")
-        end)
-
-        nvim.lsp.on_supports_method(methods.textDocument_codeLens, function()
-            --
-            ev.on({ ev.BufEnter, ev.CursorHold, ev.InsertLeave }, function()
-                if vim.g.codelens then
-                    vim.lsp.codelens.refresh({ bufnr = 0 })
-                end
-            end, {
-                group = ev.group("vim.lsp.codelens.refresh", true),
-            })
-
-            Snacks.toggle({
-                name = "Code Lens",
-                get = function()
-                    return vim.g.codelens
-                end,
-                set = function(state)
-                    vim.g.codelens = state
-
-                    if state == true then
-                        vim.lsp.codelens.refresh()
-                    else
-                        vim.lsp.codelens.clear()
+            nvim.lsp.on_supports_method(methods.textDocument_codeLens, function()
+                --
+                ev.on({ ev.BufEnter, ev.CursorHold, ev.InsertLeave }, function()
+                    if vim.g.codelens then
+                        vim.lsp.codelens.refresh({ bufnr = 0 })
                     end
-                end,
-            }):map("<space>tL")
-        end)
+                end, {
+                    group = ev.group("vim.lsp.codelens.refresh", true),
+                })
 
-        local capabilities = nil
+                Snacks.toggle({
+                    name = "Code Lens",
+                    get = function()
+                        return vim.g.codelens
+                    end,
+                    set = function(state)
+                        vim.g.codelens = state
 
-        if pcall(require, "blink.cmp") then
-            capabilities = require("blink.cmp").get_lsp_capabilities({
-                workspace = {
-                    didChangeWatchedFiles = {
-                        dynamicRegistration = true,
+                        if state == true then
+                            vim.lsp.codelens.refresh()
+                        else
+                            vim.lsp.codelens.clear()
+                        end
+                    end,
+                }):map("<space>tL")
+            end)
+
+            local capabilities = nil
+
+            if pcall(require, "blink.cmp") then
+                capabilities = require("blink.cmp").get_lsp_capabilities({
+                    workspace = {
+                        didChangeWatchedFiles = {
+                            dynamicRegistration = true,
+                        },
                     },
-                },
-            })
-        end
+                })
+            end
 
-        -- Set defaults
-        vim.lsp.config("*", {
-            capabilities = capabilities,
-            root_markers = { ".git" },
-        } --[[@as vim.lsp.Config]])
+            -- Set defaults
+            vim.lsp.config("*", {
+                capabilities = capabilities,
+                root_markers = { ".git" },
+            } --[[@as vim.lsp.Config]])
 
-        local configured = {
-            "codebook",
-            "config-lsp",
-            "dockerls",
-            "emmylua_ls",
-            "fish-ls",
-            "gopls",
-            "harper_ls",
-            "helm_ls",
-            "superhtml",
-            "jinja_lsp",
-            "jsonls",
-            "just-lsp",
-            "lemminx",
-            "marksman",
-            "nil_ls",
-            "nixd",
-            "pkl",
-            "pyrefly",
-            "ruff",
-            "sourcekit",
-            "systemd_ls",
-            "tombi",
-            "ts_query_ls",
-            "tsgo",
-            "ty",
-            "yamlls",
-            "zls",
-        }
+            local configured = {
+                "codebook",
+                "config-lsp",
+                "dockerls",
+                "emmylua_ls",
+                "fish-ls",
+                "gopls",
+                "harper_ls",
+                "helm_ls",
+                "superhtml",
+                "jinja_lsp",
+                "jsonls",
+                "just-lsp",
+                "lemminx",
+                "marksman",
+                "nil_ls",
+                "nixd",
+                "pkl",
+                "pyrefly",
+                "ruff",
+                "sourcekit",
+                "systemd_ls",
+                "tombi",
+                "ts_query_ls",
+                "tsgo",
+                "ty",
+                "yamlls",
+                "zls",
+            }
 
-        vim.iter(configured):each(vim.schedule_wrap(function(server_name)
-            -- if disabled[server_name] then
-            --     return
-            -- end
-            vim.lsp.enable(server_name)
-        end))
-    end,
+            vim.iter(configured):each(vim.schedule_wrap(function(server_name)
+                -- if disabled[server_name] then
+                --     return
+                -- end
+                vim.lsp.enable(server_name)
+            end))
+        end,
+    },
 }
