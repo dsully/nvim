63d08d051e2017a19b476b1da6bfeff1f5c67ab7
Dan Sully <dsully@users.noreply.github.com>
2025-09-27 11:36:21 -0700

    refactor: schemastore language servers

--- after/lsp/helm_ls.lua
+++ after/lsp/helm_ls.lua
@@ -1,59 +1,49 @@
 -- https://github.com/someone-stole-my-name/schema-companion.nvim/issues/12#issuecomment-1367850121
 ---@type vim.lsp.ClientConfig
-return {
-    override = function(config)
-        return require("schema-companion").setup_client(
-            require("schema-companion").adapters.helmls.setup({
-                sources = {
-                    require("schema-companion").sources.matchers.kubernetes.setup({ version = "master" }),
-                    require("schema-companion").sources.lsp.setup(),
-                    require("schema-companion").sources.schemas.setup({
-                        {
-                            name = "Kubernetes master",
-                            uri = "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/master-standalone-strict/all.json",
-                        },
-                        {
-                            name = "Kubernetes v1.27",
-                            uri = "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.27.16-standalone-strict/all.json",
-                        },
-                        {
-                            name = "Kubernetes v1.28",
-                            uri = "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.28.12-standalone-strict/all.json",
-                        },
-                        {
-                            name = "Kubernetes v1.29",
-                            uri = "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.29.7-standalone-strict/all.json",
-                        },
-                        {
-                            name = "Kubernetes v1.30",
-                            uri = "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.30.3-standalone-strict/all.json",
-                        },
-                    }),
-                },
-            }),
-            config
-        )
-    end,
-    settings = {
-        flags = {
-            debounce_text_changes = 50,
+return require("schema-companion").setup_client(
+    require("schema-companion").adapters.helmls.setup({
+        sources = {
+            require("schema-companion").sources.lsp.setup(),
+            require("schema-companion").sources.matchers.kubernetes.setup({ version = "master" }),
         },
-        ["helm-ls"] = {
-            yamlls = {
-                enabled = true,
-                diagnosticsLimit = 50,
-                showDiagnosticsDirectly = false,
-                path = "yaml-language-server",
-                config = {
-                    validate = true,
-                    format = { enable = true },
-                    completion = true,
-                    hover = true,
-                    schemaDownload = { enable = true },
-                    schemaStore = { enable = true, url = "https://www.schemastore.org/api/json/catalog.json" },
-                    -- any other config: https://github.com/redhat-developer/yaml-language-server#language-server-settings
+    }),
+    ---@diagnostic disable-next-line: param-type-not-match
+    {
+        --- @param client vim.lsp.Client
+        on_init = function(client)
+            local schemas = nvim.file.read(nvim.file.xdg_config("schemas.json")) or "{}"
+
+            ---@diagnostic disable-next-line: inject-field, need-check-nil
+            client.config.settings.json.schemas = require("schemastore").json.schemas({
+                extra = vim.json.decode(schemas),
+            })
+
+            client:notify(vim.lsp.protocol.Methods.workspace_didChangeConfiguration, { settings = client.config.settings })
+        end,
+        settings = {
+            flags = {
+                debounce_text_changes = 50,
+            },
+            ["helm-ls"] = {
+                yamlls = {
+                    enabled = true,
+                    diagnosticsLimit = 50,
+                    showDiagnosticsDirectly = false,
+                    path = "yaml-language-server",
+                    config = {
+                        completion = true,
+                        format = {
+                            enable = true,
+                        },
+                        hover = true,
+                        schemas = {},
+                        schemaDownload = {
+                            enable = true,
+                        },
+                        validate = true,
+                    },
                 },
             },
         },
-    },
-}
+    }
+)
--- after/lsp/taplo.lua
+++ after/lsp/taplo.lua
@@ -1,17 +1,22 @@
 ---@type vim.lsp.ClientConfig
-return {
-    override = function(config)
-        return require("schema-companion").setup_client(
-            require("schema-companion").adapters.taplo.setup({
-                sources = {
-                    require("schema-companion").sources.lsp.setup(),
-                    require("schema-companion").sources.none.setup(),
-                },
-            }),
-            config
-        )
-    end,
-    settings = {
-        settings = {},
-    },
-}
+return require("schema-companion").setup_client(
+    require("schema-companion").adapters.taplo.setup({
+        sources = {
+            require("schema-companion").sources.lsp.setup(),
+            require("schema-companion").sources.none.setup(),
+        },
+    }),
+    ---@diagnostic disable-next-line: param-type-not-match
+    {
+        --- @param client vim.lsp.Client
+        on_init = function(client)
+            keys.map("<leader>vs", function()
+                local schema = require("schema-companion").get_current_schemas()
+
+                if schema then
+                    Snacks.notify.info(schema)
+                end
+            end, "Show YAML schema")
+        end,
+    }
+)
