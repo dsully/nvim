2cba09af4fee179f543282cdf2a32c818bb2266e
Dan Sully <dsully@users.noreply.github.com>
2025-09-27 12:00:50 -0700

    fix: update schema-companion key bindings

--- lua/plugins/schema-companion.lua
+++ lua/plugins/schema-companion.lua
@@ -2,33 +2,21 @@
 return {
     {
         "cenk1cenk2/schema-companion.nvim",
-        ft = { "helm", "yaml" },
+        ft = { "yaml", "helm", "yaml.*", "json*", "toml" },
         keys = {
             {
-                "<leader>ys",
+                "<leader>vs",
                 function()
-                    local schemas = require("schema-companion.schema").all()
-
-                    if not schemas or #schemas == 0 then
-                        vim.notify("No schemas available", vim.log.levels.WARN, { title = "Schema Companion" })
-                        return
-                    end
-
-                    vim.ui.select(schemas, {
-                        prompt = "Select any available schema",
-                        format_item = function(item)
-                            return item.name or item.uri or "<unnamed>"
-                        end,
-                    }, function(choice)
-                        if choice then
-                            require("schema-companion.context").schema(vim.api.nvim_get_current_buf(), {
-                                name = choice.name or choice.uri,
-                                uri = choice.uri,
-                            })
-                        end
-                    end)
+                    Snacks.notify.info(require("schema-companion").get_current_schemas() or "none")
                 end,
-                desc = "Select a YAML Schema",
+                desc = "Show the current Schema",
+            },
+            {
+                "<leader>vS",
+                function()
+                    return require("schema-companion").select_schema()
+                end,
+                desc = "Select a Schema",
             },
         },
         opts = {},
