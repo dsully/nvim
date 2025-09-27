f2defcf974c8d8b34007248c665dbb6c7c26abaf
Dan Sully <dsully@users.noreply.github.com>
2025-09-27 09:50:53 -0700

    fix: remove unneeded fish-lsp config

--- after/lsp/fish-ls.lua
+++ after/lsp/fish-ls.lua
@@ -6,21 +6,5 @@ return {
         "fish",
         "fish.gotmpl",
     },
-    initializationOptions = {
-        workspaces = {
-            paths = {
-                defaults = {
-                    vim.env.XDG_CONFIG_HOME .. "/fish",
-                    vim.env.HOMEBREW_PREFIX .. "/share/fish/",
-                },
-            },
-        },
-    },
-    ---@param client vim.lsp.Client
-    on_attach = function(client)
-        if client.server_capabilities then
-            client.server_capabilities.documentHighlightProvider = false
-        end
-    end,
     single_file_support = true,
 }
