d0f8ea2351422c86c4aff86ebe36fafdb9e5cf7d
Dan Sully <dsully@users.noreply.github.com>
2025-11-19 04:55:32 -0800

    fix(fish-lsp): weird commit interaction

--- after/lsp/fish-ls.lua
+++ after/lsp/fish-ls.lua
@@ -1,15 +1,13 @@
 ---@type vim.lsp.Config
 return {
     cmd = { "fish-lsp", "start" },
-    cmd_env = { fish_lsp_show_client_popups = false },
+    cmd_env = {
+        fish_lsp_commit_characters = "'\t'",
+        fish_lsp_show_client_popups = false,
+    },
     filetypes = {
         "fish",
         "fish.gotmpl",
     },
-    on_attach = function(client)
-        if client.server_capabilities then
-            client.server_capabilities.documentOnTypeFormattingProvider = nil
-        end
-    end,
     single_file_support = true,
 }
