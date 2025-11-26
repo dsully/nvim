312f885651e7f54e0479f2b9b1aa00be0db25b0b
Dan Sully <dsully@users.noreply.github.com>
2025-11-25 19:52:33 -0800

    chore: remove kubectl

--- lua/plugins/k8s.lua
+++ lua/plugins/k8s.lua
@@ -1,32 +1,6 @@
 ---@type LazySpec[]
 return {
     {
-        "ramilito/kubectl.nvim",
-        -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-        -- build = 'cargo build --release',
-        dependencies = "saghen/blink.download",
-        keys = {
-            {
-                "<leader>k",
-                function()
-                    require("kubectl").toggle({ tab = false })
-                end,
-                desc = "Kubectl",
-            },
-            -- https://github.com/Ramilito/kubectl.nvim/wiki/Lazy-setup
-            { "<C-k>", "<Plug>(kubectl.kill)", ft = "k8s_*" },
-            { "7", "<Plug>(kubectl.view_nodes)", ft = "k8s_*" },
-            { "8", "<Plug>(kubectl.view_overview)", ft = "k8s_*" },
-            { "<C-t>", "<Plug>(kubectl.view_top)", ft = "k8s_*" },
-        },
-        opts = {
-            statusline = {
-                enabled = true,
-            },
-        },
-        version = "2.*",
-    },
-    {
         "diogo464/kubernetes.nvim",
         ft = { "yaml" },
         opts = {
