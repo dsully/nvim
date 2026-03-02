4f5771ded7fcfd4b5f3947c8d402628f62d527c4
Dan Sully <dsully@users.noreply.github.com>
2026-03-02 09:11:13 -0800

    feat(python) Try smellycat

--- lua/plugins/languages/python.lua
+++ lua/plugins/languages/python.lua
@@ -44,4 +44,26 @@ return {
             },
         },
     },
+    {
+        "https://codeberg.org/mraspaud/smellycat.nvim",
+        ft = "python",
+        opts = {
+            debounce_ms = 500,
+            smells = {
+                arrow_code = { enabled = true, max_depth = 4 },
+                brain_method = { enabled = true, max_lines = 50, max_branches = 10 },
+                data_class = { enabled = true, min_non_dunder_methods = 1 },
+                data_clumps = { enabled = true, min_clump_size = 3, min_occurrences = 2 },
+                feature_envy = { enabled = true, external_call_ratio_threshold = 0.75 },
+                god_object = { enabled = true, max_methods = 20, max_references = 100 },
+                inappropriate_intimacy = { enabled = true, min_private_accesses = 1 },
+                lazy_class = { enabled = true, min_methods = 2 },
+                long_parameter_list = { enabled = true, max_params = 5 },
+                message_chains = { enabled = true, max_chain_length = 3 },
+                middle_man = { enabled = true, min_delegation_ratio = 0.75 },
+                switch_statements = { enabled = true, min_cases = 5 },
+            },
+            max_concurrent_lsp_requests = 4,
+        },
+    },
 }
