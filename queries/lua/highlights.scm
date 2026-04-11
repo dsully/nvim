adb15e73b931e6fdb7c27a244bbf40be39b91bb4
Dan Sully <dsully@users.noreply.github.com>
2026-04-10 20:05:05 -0700

    chore: Consolidate builtins

--- queries/lua/highlights.scm
+++ queries/lua/highlights.scm
@@ -1,25 +1,5 @@
 ; extends
 
 ((identifier) @module.builtin
-  (#eq? @module.builtin "colors"))
-
-((identifier) @module.builtin
-  (#eq? @module.builtin "defaults"))
-
-((identifier) @module.builtin
-  (#eq? @module.builtin "ev"))
-
-((identifier) @module.builtin
-  (#eq? @module.builtin "hl"))
-
-((identifier) @module.builtin
-  (#eq? @module.builtin "keys"))
-
-((identifier) @module.builtin
-  (#eq? @module.builtin "lazy"))
-
-((identifier) @module.builtin
-  (#eq? @module.builtin "nvim"))
-
-((identifier) @module.builtin
-  (#eq? @module.builtin "vim"))
+  (#any-of? @module.builtin
+    "colors" "defaults" "ev" "hl" "keys" "lazy" "nvim" "vim"))
