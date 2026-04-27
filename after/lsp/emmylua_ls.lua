922f9cd731c9823d35e47402e191face5e3d2eb3
Dan Sully <dsully@users.noreply.github.com>
2026-04-27 08:13:09 -0700

    fix(lsp): Only run emmylua in ~/.config/nvim

--- after/lsp/emmylua_ls.lua
+++ after/lsp/emmylua_ls.lua
@@ -4,16 +4,14 @@ return {
         "emmylua_ls",
     },
     filetypes = { "lua" },
-    root_markers = {
-        ".emmyrc.json",
-        ".luarc.json",
-        ".luarc.jsonc",
-        ".stylua.toml",
-        "lazy-lock.json",
-        "selene.toml",
-        "selene.yml",
-        "stylua.toml",
-        "lua/",
-    },
-    single_file_support = true,
+    root_dir = function(bufnr, on_dir)
+        local nvim_config = nvim.file.realpath(nvim.file.xdg_config("nvim"))
+        local bufname = nvim.file.filename(bufnr)
+
+        if bufname == nvim_config or vim.startswith(bufname, nvim_config .. "/") then
+            on_dir(nvim_config)
+        end
+    end,
+    single_file_support = false,
+    workspace_required = true,
 }
