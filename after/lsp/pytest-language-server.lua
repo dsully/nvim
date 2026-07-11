833b5ba2559676a2b803d97ad063c3256025200b
Dan Sully <dsully@users.noreply.github.com>
2026-07-11 08:52:39 -0700

    chore(lsp): pytest-language-server is now part of nvim-lspconfig

--- after/lsp/pytest-language-server.lua
+++ after/lsp/pytest-language-server.lua
@@ -1,20 +1,10 @@
 ---@type vim.lsp.Config
 return {
-    cmd = { "pytest-language-server" },
-    filetypes = {
-        "python",
-    },
     ---@param client vim.lsp.Client
     on_attach = function(client)
         if client.server_capabilities then
             client.server_capabilities.hoverProvider = nil
         end
     end,
-    root_markers = {
-        "pyproject.toml",
-        "pytest.ini",
-        "requirements.txt",
-        "setup.py",
-    },
     single_file_support = true,
 }
