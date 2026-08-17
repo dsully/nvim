7b3a1555c1c46ec07f37f8e3c90963fe0fbc83ae
Dan Sully <dsully@users.noreply.github.com>
2026-08-16 18:19:22 -0700

    chore: tsgo -> tsc

--- after/lsp/tsc.lua
+++ after/lsp/tsc.lua
@@ -1,6 +1,5 @@
 ---@type vim.lsp.Config
 return {
-    cmd = { "tsgo", "--lsp", "--stdio" },
     filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
     root_dir = function(bufnr, on_dir)
         local root_markers = { { "package-lock.json", "yarn.lock", "pnpm-lock.yaml" }, { ".git" } }
